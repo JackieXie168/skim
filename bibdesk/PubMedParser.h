a47ffe4701c0570b8fcbe89c136b02ce86e1f635
mhohensee <mhohensee@lists.sourceforge.net>
2006-12-07 15:03:50 +0000

    Change version to 564

---
+++ bibdesk/PubMedParser.h
@@ -0,0 +1,23 @@
+//
+//  PubMedParser.h
+//  Bibdesk
+//
+//  Created by Michael McCracken on Sun Nov 16 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BibItem.h"
+#import "BibAppController.h"
+#import "AGRegex/AGRegex.h"
+
+@interface PubMedParser : NSObject {
+
+}
+
+
++ (NSMutableArray *)itemsFromString:(NSString *)itemString error:(BOOL *)hadProblems;
++ (NSMutableArray *)itemsFromString:(NSString *)itemString error:(BOOL *)hadProblems frontMatter:(NSMutableString *)frontMatter filePath:(NSString *)filePath;
+
+
+@end
