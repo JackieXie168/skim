e38c083dfaecd2b24f0e67403f6450428e5ee106
mhohensee <mhohensee@lists.sourceforge.net>
2006-12-07 15:03:50 +0000

    Change version to 564

---
+++ bibdesk/Scripting/BibAuthor+Scripting.h
@@ -0,0 +1,17 @@
+//
+//  BibAuthorScripting.h
+//  BibDesk
+//
+//  Created by Sven-S. Porst on Sat Jul 10 2004.
+//  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BibAuthor.h"
+#import "BibItem+Scripting.h"
+
+@interface BibAuthor (Scripting)
+
+- (NSArray *)publications;
+
+@end
