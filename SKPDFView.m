5262f1419260b2623bc19a7a6927446957c86a6c
hofman <hofman@gmail.com>
2006-12-24 16:36:34 +0000

    Include OmniAppkit methods as we don't include the framework

--- SKPDFView.m
+++ SKPDFView.m
@@ -99,7 +99,8 @@ NSString *SKPDFViewToolModeChangedNotification = @"SKPDFViewToolModeChangedNotif
             [self magnifyWithEvent:theEvent];
             break;
         case SKPopUpToolMode:
-            [self popUpWithEvent:theEvent];
+            //[self popUpWithEvent:theEvent];
+            [self selectWithEvent:theEvent];
             break;
         case SKAnnotateToolMode:
             [super mouseDown:theEvent];
@@ -363,6 +364,10 @@ NSString *SKPDFViewToolModeChangedNotification = @"SKPDFViewToolModeChangedNotif
                     NSIntersectionRect(NSInsetRect(magRect, -2.0, -2.0), [[self superview] convertRect:[[self superview] bounds] toView:nil])];
             }
             
+            // draw marquee
+            if (selRectTimer)
+                [self updateMarquee:nil];
+            
             // resize bounds around mouseLoc
             magBounds.origin = [[self documentView] convertPoint:mouseLoc fromView:nil];
             magBounds = NSMakeRect(magBounds.origin.x + magScale * (originalBounds.origin.x - magBounds.origin.x), 
@@ -397,6 +402,241 @@ NSString *SKPDFViewToolModeChangedNotification = @"SKPDFViewToolModeChangedNotif
 	[NSCursor unhide];
 	[[self documentView] setPostsBoundsChangedNotifications:postNote];
 	[self flagsChanged:theEvent]; // update cursor
+	[self cleanupMarquee:NO];
+	[self recacheMarquee]; 
+}
+
+#define SIZE_WINDOW_H_OFFSET	75
+#define SIZE_WINDOW_V_OFFSET	-10
+#define SIZE_WINDOW_WIDTH		70
+#define SIZE_WINDOW_HEIGHT		20
+#define SIZE_WINDOW_DRAW_X		5
+#define SIZE_WINDOW_DRAW_Y		3
+#define SIZE_WINDOW_HAS_SHADOW	NO
+
+- (void)selectWithEvent:(NSEvent *)theEvent {
+    NSPoint mouseLoc, startPoint, currentPoint;
+	NSRect bounds, selRectWindow, selRectSuper;
+	NSBezierPath *path = [NSBezierPath bezierPath];
+	BOOL startFromCenter = NO;
+	static int phase = 0;
+	float xmin, xmax, ymin, ymax, pattern[] = {3,3};
+	
+	[path setLineWidth: 0.01];
+	mouseLoc = [theEvent locationInWindow];
+	startPoint = [[self documentView] convertPoint:mouseLoc fromView:nil];
+	[NSEvent startPeriodicEventsAfterDelay:0.0 withPeriod: 0.2];
+	[self cleanupMarquee:YES];
+	[[self window] discardCachedImage];
+	
+#ifndef DO_NOT_SHOW_SELECTION_SIZE
+	// create a small window displaying the size of selection
+	NSRect aRect;
+	aRect.origin = [[self window] convertBaseToScreen: mouseLoc];
+	aRect.origin.x -= SIZE_WINDOW_H_OFFSET;
+	aRect.origin.y += SIZE_WINDOW_V_OFFSET;
+	aRect.size = NSMakeSize(SIZE_WINDOW_WIDTH, SIZE_WINDOW_HEIGHT);
+	NSPanel *sizeWindow = [[NSPanel alloc] initWithContentRect: aRect 
+													 styleMask: NSBorderlessWindowMask | NSUtilityWindowMask 
+													   backing: NSBackingStoreBuffered //NSBackingStoreRetained 
+														 defer: NO];
+	[sizeWindow setOpaque: NO];
+	[sizeWindow setHasShadow: SIZE_WINDOW_HAS_SHADOW];
+	[sizeWindow orderFront: nil];
+	[sizeWindow setFloatingPanel: YES];
+#endif
+	
+	while ([theEvent type] != NSLeftMouseUp) {
+        // restore the cached image in order to clear the rect
+        [[self window] restoreCachedImage];
+        [[self window] flushWindow];
+        // get Mouse location and check if it is with the view's rect
+        if ([theEvent type] == NSLeftMouseDragged || [theEvent type] == NSLeftMouseDown) {
+            mouseLoc = [theEvent locationInWindow];
+            // scroll if the mouse is out of visibleRect
+            [[self documentView] autoscroll: theEvent];
+        }
+        // calculate the rect to select
+        currentPoint = [[self documentView] convertPoint: mouseLoc fromView:nil];
+        selectedRect.size = NSMakeSize(abs(currentPoint.x - startPoint.x), abs(currentPoint.y - startPoint.y));
+        if ([theEvent modifierFlags] & NSShiftKeyMask) {
+            if (NSWidth(selectedRect) > NSHeight(selectedRect))
+                selectedRect.size.height = NSWidth(selectedRect);
+            else
+                selectedRect.size.width = NSHeight(selectedRect);
+        }
+        if (currentPoint.x < startPoint.x || startFromCenter)
+            selectedRect.origin.x = startPoint.x - NSWidth(selectedRect);
+        else
+            selectedRect.origin.x = startPoint.x;
+        if (currentPoint.y < startPoint.y || startFromCenter)
+            selectedRect.origin.y = startPoint.y - NSHeight(selectedRect);
+        else
+            selectedRect.origin.y = startPoint.y;
+        if (startFromCenter) {
+            selectedRect.size.width *= 2;
+            selectedRect.size.height *= 2;
+        }
+        // calculate the intersection of selectedRect with bounds 
+        // -- we do not want to use NSIntersectionRect 
+        // because even if it's empty, we want information on origin and edges
+        // in our case, the only way the intersection can be empty is that 
+        // one of the edges has length zero.  
+        bounds = [[self documentView] bounds];
+        xmin = fmax(NSMinX(selectedRect), NSMinX(bounds));
+        xmax = fmin(NSMaxX(selectedRect), NSMaxX(bounds));
+        ymin = fmax(NSMinY(selectedRect), NSMinY(bounds));
+        ymax = fmin(NSMaxY(selectedRect), NSMaxY(bounds));
+        selectedRect = NSMakeRect(xmin, ymin, xmax - xmin, ymax - ymin);
+        // do not use selectedRect = NSIntersectionRect(selectedRect, [self bounds]);
+        selRectWindow = [[self documentView] convertRect: selectedRect toView: nil];
+        // cache the window image
+        [[self window] cacheImageInRect:NSInsetRect(selRectWindow, -2.0, -2.0)];
+        // draw rect frame
+        [path removeAllPoints]; // reuse path
+                                // in order to draw a clear frame we draw an adjusted rect in clip view
+        selRectSuper = [self convertRect:selRectWindow fromView: nil];
+        if (NO == NSIsEmptyRect(selRectSuper)) {	// shift the coordinated by a half integer
+            selRectSuper = NSInsetRect(NSIntegralRect(selRectSuper), 0.5, 0.5);
+            [path appendBezierPathWithRect: selRectSuper];
+        } else { // if width or height is zero, we cannot use NSIntegralRect, which returns zero rect, so draw a path by hand
+            selRectSuper.origin.x = floor(NSMinX(selRectSuper)) + 0.5;
+            selRectSuper.origin.y = floor(NSMinY(selRectSuper)) + 0.5;
+            [path appendBezierPathWithPoints: &(selRectSuper.origin) count: 1];
+            selRectSuper.origin.x += floor(NSWidth(selRectSuper));
+            selRectSuper.origin.y += floor(NSHeight(selRectSuper));
+            [path appendBezierPathWithPoints: &(selRectSuper.origin) count: 1];
+        }
+        [self lockFocus];
+        [[NSGraphicsContext currentContext] setShouldAntialias: NO];
+        [[NSColor whiteColor] set];
+        [path stroke];
+        [path setLineDash: pattern count: 2 phase: phase];
+        [[NSColor blackColor] set];
+        [path stroke];
+        phase = (phase + 1) % 6;
+        [self unlockFocus];
+        // display the image drawn in the buffer
+        [[self window] flushWindow];
+        
+#ifndef DO_NOT_SHOW_SELECTION_SIZE
+        
+        // update the size window
+        // first compute where to show the window
+        NSRect contentRect = [[[self window] contentView] bounds];
+        xmin = fmax(NSMinX(selRectWindow), NSMinX(contentRect));
+        ymin = fmax(NSMinY(selRectWindow), NSMinY(contentRect));
+        ymax = fmin(NSMaxY(selRectWindow), NSMaxY(contentRect));
+        NSPoint aPoint = NSMakePoint(xmin, 0.5 * (ymin + ymax));
+        aPoint = [[self window] convertBaseToScreen: aPoint];
+        aPoint.x -= SIZE_WINDOW_H_OFFSET;
+        aPoint.y += SIZE_WINDOW_V_OFFSET;
+        [sizeWindow setFrameOrigin: aPoint]; // set the position
+                                             // do the drawing
+        NSString *sizeString = [NSString stringWithFormat: @"%d x %d", (int)floor(NSWidth(selRectWindow)), (int)floor(NSHeight(selRectWindow))];
+        NSView *sizeView = [sizeWindow contentView];
+        [sizeView lockFocus];
+        [[NSColor colorWithCalibratedRed:1.0 green:1.0 blue:0.5 alpha:0.8] set];//change color?
+        NSRectFill([sizeView bounds]);
+        [sizeString drawAtPoint: NSMakePoint(SIZE_WINDOW_DRAW_X, SIZE_WINDOW_DRAW_Y) withAttributes: [NSDictionary dictionary]];
+        [[NSGraphicsContext currentContext] flushGraphics];
+        [sizeView unlockFocus];
+            
+#endif
+        
+        theEvent = [[self window] nextEventMatchingMask: NSLeftMouseUpMask | NSLeftMouseDraggedMask | NSFlagsChangedMask | NSPeriodicMask];
+	}
+	
+	[NSEvent stopPeriodicEvents];
+	if (NSWidth(selectedRect) > 3.0 && NSHeight(selectedRect) > 3.0) {
+		selRectTimer = [NSTimer scheduledTimerWithTimeInterval: 0.2 target:self 
+													  selector:@selector(updateMarquee:) userInfo:nil repeats:YES];
+		oldVisibleRect = [[self documentView] visibleRect];
+	} else {
+		selRectTimer = nil;
+		[[self window] restoreCachedImage];
+		[[self window] flushWindow];
+		[[self window] discardCachedImage];
+	}
+	[self flagsChanged: theEvent]; // update cursor
+#ifndef DO_NOT_SHOW_SELECTION_SIZE
+	[sizeWindow close];
+#endif
+}
+
+// updates the frame of selected rectangle
+- (void)updateMarquee:(NSTimer *)timer {
+	static int phase = 0;
+	float pattern[2] = {3,3};
+	NSView *clipView;
+	NSRect selRectSuper, clipBounds;
+	NSBezierPath *path;
+	
+	if ([[self window] isMainWindow] == NO)
+        return;
+	
+    clipView = [[self documentView] superview];
+    clipBounds = [clipView bounds];
+    selRectSuper = [[self documentView] convertRect:selectedRect toView: clipView];
+    selRectSuper = NSInsetRect(NSIntegralRect(selRectSuper), 0.5, 0.5);
+    // if the eddges are slightly off the clip view, adjust them.
+    if (fabs(NSMinX(clipBounds) - NSMinX(selRectSuper) - 0.5) < 0.5) {
+        selRectSuper.origin.x += 1.0;
+        selRectSuper.size.width -= 1.0;
+    } else if (fabs(NSMaxX(clipBounds) - NSMaxX(selRectSuper) + 0.5) < 0.5) {
+        selRectSuper.size.width -= 1.0;
+    }
+    if (fabs(NSMinY(clipBounds) - NSMinY(selRectSuper) - 0.5) < 0.5) {
+        selRectSuper.origin.y += 1.0;
+        selRectSuper.size.height -= 1.0;
+    } else if (fabs(NSMaxY(clipBounds) - NSMaxY(selRectSuper) + 0.5) < 0.5) {
+        selRectSuper.size.height -= 1.0;
+    }
+    // create a bezier path and draw
+    [clipView lockFocus];
+    [[NSGraphicsContext currentContext] setShouldAntialias: NO];
+    path = [NSBezierPath bezierPathWithRect: selRectSuper];
+    [path setLineWidth: 0.01];
+    [[NSColor whiteColor] set];
+    [path stroke];
+    [path setLineDash: pattern count: 2 phase: phase];
+    [[NSColor blackColor] set];
+    [path stroke];
+    [clipView unlockFocus];
+    if (timer)
+        [[self window] flushWindow];
+    phase = (phase + 1) % 6;
+}
+
+// erases the frame of selected rectangle and cleans up the cached image
+- (void)cleanupMarquee:(BOOL)terminate {
+	if (selRectTimer == nil) 
+        return;
+    
+    NSRect tempRect, visRect = [[self documentView] visibleRect];
+    if (NSEqualRects(visRect, oldVisibleRect)) {
+        [[self window] restoreCachedImage];
+        [[self window] flushWindow];
+    } else { // the view was moved--do not use the cached image
+        [[self window] discardCachedImage];
+        tempRect =  [self convertRect: NSInsetRect(NSIntegralRect([[self documentView] convertRect: selectedRect toView: nil]), -2.0, -2.0) 
+                             fromView: nil];
+        [self displayRect: tempRect];
+    }
+    oldVisibleRect.size.width = 0.0; // do not use this cache again
+    if (terminate) {
+        [selRectTimer invalidate]; // this will release the timer
+        selRectTimer = nil;
+	}
+}
+
+// recache the image around selected rectangle for quicker response
+- (void)recacheMarquee
+{
+	if (selRectTimer == nil)
+        return;
+    [[self window] cacheImageInRect: NSInsetRect([[self documentView] convertRect: selectedRect toView: nil], -2.0, -2.0)];
+    oldVisibleRect = [self visibleRect];
 }
 
 @end
