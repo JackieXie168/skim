00b9235be0eb79a87331b76496892a42a7fb30d4
amaxwell <amaxwell@gmail.com>
2006-12-27 22:23:12 +0000

    Add default.bib file from YAZ for search mappings.  Not sure what servers this is compatible with, but we'll load it by default for now.
    Fix crasher when query is nil, and assert parameter to catch that
    in future.
    
    Add a formatter subclass that can be used to catch unparseable CCL
    query strings.

--- bibdesk_vendorsrc/indexdata/yaz/BDSKZoomQuery.m
+++ bibdesk_vendorsrc/indexdata/yaz/BDSKZoomQuery.m
@@ -8,7 +8,6 @@
 
 #import "BDSKZoomQuery.h"
 
-
 @implementation BDSKZoomQuery
 
 + (id)queryWithCCLString:(NSString *)queryString config:(NSString *)confString;
@@ -16,20 +15,51 @@
     return [[[self allocWithZone:[self zone]] initWithCCLString:queryString config:confString] autorelease];
 }
 
++ (NSString *)defaultConfigString;
+{
+    static NSString *config = nil;
+    if (nil == config) {
+        NSBundle *bundle = [NSBundle bundleWithIdentifier:@"net.sourceforge.bibdesk.yaz"];
+        config = [[NSString alloc] initWithContentsOfFile:[bundle pathForResource:@"default" ofType:@"bib"] encoding:NSASCIIStringEncoding error:NULL];
+        if (nil == config) config = [@"" copy];
+    }
+    return config;
+}
+
++ (const char *)defaultConfigCString;
+{
+    static const char *config = NULL;
+    if (NULL == config) {
+        NSString *str = [self defaultConfigString];
+        unsigned len = ([str lengthOfBytesUsingEncoding:NSASCIIStringEncoding] + 1);
+        char *buf = NSZoneMalloc(NULL, sizeof(char) * len);
+        [str getCString:buf maxLength:len encoding:NSASCIIStringEncoding];
+        config = buf ? buf : "";
+    }
+    return config;
+}
+
 - (id)initWithCCLString:(NSString *)queryString config:(NSString *)confString;
 {
     self = [super init];
     if (self){
-        if (nil == confString)
-            confString = @"term t=l,r s=al\n" "ti u=4 s=pw\n";
+        
+        const char *conf = NULL;
+        
+        if (confString) {
+            _config = [confString copy];
+            conf = [confString UTF8String];
+        } else {
+            _config = [[[self class] defaultConfigString] copy];
+            conf = [[self class] defaultConfigCString];
+        }
         
         _query = ZOOM_query_create();
-        _config = [confString copy];
         _queryString = [queryString copy];
         
         int status, error, errorPosition;
         const char *errstring;
-        status = ZOOM_query_ccl2rpn(_query, [_queryString UTF8String], [_config UTF8String], &error, &errstring, &errorPosition);
+        status = ZOOM_query_ccl2rpn(_query, [_queryString UTF8String], conf, &error, &errstring, &errorPosition);
         if (status) {
             [self release];
             self = nil;
@@ -67,3 +97,33 @@
 - (ZOOM_query)zoomQuery { return _query; }
 
 @end
+
+        
+@implementation BDSKZoomCCLQueryFormatter
+
+- (NSString *)stringForObjectValue:(id)obj { return obj; }
+
+- (BOOL)getObjectValue:(id *)obj forString:(NSString *)string errorDescription:(NSString **)error;
+{
+    BOOL success;
+    if (string && [@"" isEqualToString:string] == NO) {
+        int status, err, errorPosition;
+        const char *errstring;
+        ZOOM_query query = ZOOM_query_create();
+        status = ZOOM_query_ccl2rpn(query, [string UTF8String], [BDSKZoomQuery defaultConfigCString], &err, &errstring, &errorPosition);
+        if (status) {
+            if (error) *error = [NSString stringWithFormat:@"%s (at position %d).", errstring, errorPosition];
+            success = NO;
+        } else {
+            success = YES;
+        }
+        ZOOM_query_destroy(query);
+    } else {
+        success = YES;
+    }
+    *obj = string;
+    return success;
+}
+
+@end
+
