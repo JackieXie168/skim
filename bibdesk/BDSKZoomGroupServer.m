dcf252c906ad0752cdc7321bb363af8460b0ee65
hofman <hofman@gmail.com>
2006-12-29 16:37:47 +0000

    Use thread safe dictionary instead of separate accessors and rwlock for serverInfo.

--- bibdesk/BDSKZoomGroupServer.m
+++ bibdesk/BDSKZoomGroupServer.m
@@ -9,6 +9,7 @@
 #import "BDSKZoomGroupServer.h"
 #import "BDSKSearchGroup.h"
 #import "BDSKStringParser.h"
+#import "BDSKThreadSafeMutableDictionary.h"
 
 // !!! maximum download of 25 is low, but this is really slow for some reason, at least using my canonical "bob dylan" search
 #define MAX_RESULTS 25
@@ -22,15 +23,12 @@
     self = [super init];
     if (self) {
         group = aGroup;
-        host = [[info objectForKey:@"host"] copy];
-        port = [[info objectForKey:@"port"] intValue];
-        database = [[info objectForKey:@"database"] copy];
+        serverInfo = [[BDSKThreadSafeMutableDictionary alloc] initWithCapacity:4];
         flags.failedDownload = 0;
         flags.isRetrieving = 0;
         flags.needsReset = 1;
         availableResults = 0;
         fetchedResults = 0;
-        pthread_rwlock_init(&infolock, NULL);
     }
     return self;
 }
@@ -39,13 +37,7 @@
 {
     group = nil;
     [connection release], connection = nil;
-    pthread_rwlock_wrlock(&infolock);
-    [host release], host = nil;
-    [database release], database = nil;
-    [user release], user = nil;
-    [password release], password = nil;
-    pthread_rwlock_unlock(&infolock);
-    pthread_rwlock_destroy(&infolock);
+    [serverInfo release], serverInfo = nil;
     [super dealloc];
 }
 
@@ -77,26 +69,16 @@
         [self performSelector:_cmd withObject:nil afterDelay:0.1];
 }
 
-// @@ should the password/username be included in the serverInfo?
 - (void)setServerInfo:(NSDictionary *)info;
 {
-    [self setHost:[info objectForKey:@"host"]];
-    [self setPort:[[info objectForKey:@"port"] intValue]];
-    [self setDatabase:[info objectForKey:@"database"]];
-    [self setPassword:[info objectForKey:@"password"]];
-    [self setUser:[info objectForKey:@"username"]];
+    [serverInfo setDictionary:info];
+    [serverInfo setObject:[NSNumber numberWithInt:BDSKSearchGroupZoom] forKey:@"type"];
+    [self setNeedsReset:YES];
 }
 
 - (NSDictionary *)serverInfo;
 {
-    NSMutableDictionary *info = [NSMutableDictionary dictionaryWithCapacity:3];
-    [info setValue:[NSNumber numberWithInt:BDSKSearchGroupZoom] forKey:@"type"];
-    [info setValue:[self host] forKey:@"host"];
-    [info setValue:[NSNumber numberWithInt:[(BDSKZoomGroupServer *)self port]] forKey:@"port"];
-    [info setValue:[self database] forKey:@"database"];
-    [info setValue:[self password] forKey:@"password"];
-    [info setValue:[self user] forKey:@"username"];
-    return info;
+    return [[serverInfo copy] autorelease];
 }
 
 - (void)setNumberOfAvailableResults:(int)value;
@@ -141,14 +123,18 @@
 
 - (void)resetConnection;
 {
+    NSString *host = [serverInfo objectForKey:@"host"];
+    int port = [[serverInfo objectForKey:@"port"] intValue];
+    NSString *database = [serverInfo objectForKey:@"database"];
+    NSString *password = [serverInfo objectForKey:@"password"];
+    NSString *user = [serverInfo objectForKey:@"username"];
+    
     [connection release];
-    connection = [[BDSKZoomConnection alloc] initWithHost:[self host] port:[self port] database:[self database]];
-    NSString *value = [self password];
-    if ([NSString isEmptyString:value] == NO)
-        [connection setOption:value forKey:@"password"];
-    value = [self user];
-    if ([NSString isEmptyString:value] == NO)
-        [connection setOption:value forKey:@"user"];
+    connection = [[BDSKZoomConnection alloc] initWithHost:host port:port database:database];
+    if ([NSString isEmptyString:password] == NO)
+        [connection setOption:password forKey:@"password"];
+    if ([NSString isEmptyString:user] == NO)
+        [connection setOption:user forKey:@"user"];
     OSAtomicCompareAndSwap32Barrier(1, 0, (int32_t *)&flags.needsReset);
     [self setNumberOfAvailableResults:0];
     [self setNumberOfFetchedResults:0];
@@ -226,83 +212,6 @@
 - (int)fetchedResults;
 {
     return fetchedResults;
-}
-
-- (void)setHost:(NSString *)aHost;
-{
-    pthread_rwlock_wrlock(&infolock);
-    [host autorelease];
-    host = [aHost copy];
-    pthread_rwlock_unlock(&infolock);
-    OSAtomicCompareAndSwap32Barrier(0, 1, (int32_t *)&flags.needsReset);
-}
-
-- (NSString *)host {
-    pthread_rwlock_rdlock(&infolock);
-    NSString *aHost = [[host retain] autorelease];
-    pthread_rwlock_unlock(&infolock);
-    return aHost;
-}
-
-- (void)setPort:(int)n;
-{ 
-    pthread_rwlock_wrlock(&infolock);
-    port = n; 
-    pthread_rwlock_unlock(&infolock);
-    OSAtomicCompareAndSwap32Barrier(0, 1, (int32_t *)&flags.needsReset);
-}    
-
-- (int)port {
-    pthread_rwlock_rdlock(&infolock);
-    int n = port;
-    pthread_rwlock_unlock(&infolock);
-    return n;
-}
-
-- (void)setDatabase:(NSString *)dbase;
-{
-    pthread_rwlock_wrlock(&infolock);
-    [database release];
-    database = [dbase copy];
-    pthread_rwlock_unlock(&infolock);
-    OSAtomicCompareAndSwap32Barrier(0, 1, (int32_t *)&flags.needsReset);
-}
-
-- (NSString *)database {
-    pthread_rwlock_rdlock(&infolock);
-    NSString *dbase = [[database retain] autorelease];
-    pthread_rwlock_unlock(&infolock);
-    return dbase;
-}
-
-- (void)setUser:(NSString *)aUser;
-{
-    pthread_rwlock_wrlock(&infolock);
-    [user autorelease];
-    user = [aUser copy];
-    pthread_rwlock_unlock(&infolock);
-}
-
-- (NSString *)user {
-    pthread_rwlock_rdlock(&infolock);
-    NSString *aUser = [[user retain] autorelease];
-    pthread_rwlock_unlock(&infolock);
-    return aUser;
-}
-
-- (void)setPassword:(NSString *)aPassword;
-{
-    pthread_rwlock_wrlock(&infolock);
-    [password autorelease];
-    password = [aPassword copy];
-    pthread_rwlock_unlock(&infolock);
-}
-
-- (NSString *)password { 
-    pthread_rwlock_rdlock(&infolock);
-    NSString *aPassword = [[password retain] autorelease];
-    pthread_rwlock_unlock(&infolock);
-    return aPassword;
 }
 
 @end
