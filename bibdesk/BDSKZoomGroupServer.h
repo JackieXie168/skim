c137924fe8f7d011c8743fd3baf3c85065d5cdb1
Christiaan Hofman <hofman@lists.sourceforge.net>
2006-12-29 16:56:52 +0000

    Use thread safe dictionary instead of separate accessors and rwlock for serverInfo.

--- bibdesk/BDSKZoomGroupServer.h
+++ bibdesk/BDSKZoomGroupServer.h
@@ -22,16 +22,6 @@
 - (int)fetchedResults;
 - (void)setFetchedResults:(int)value;
 - (oneway void)downloadWithSearchTerm:(NSString *)searchTerm;
-- (void)setHost:(NSString *)aHost;
-- (NSString *)host;
-- (void)setPort:(int)n;
-- (int)port;
-- (void)setDatabase:(NSString *)dbase;
-- (NSString *)database;
-- (void)setUser:(NSString *)aUser;
-- (NSString *)user;
-- (void)setPassword:(NSString *)aPassword;
-- (NSString *)password;
 - (oneway void)terminateConnection;
 @end
 
@@ -41,19 +31,17 @@ typedef struct _BDSKZoomGroupFlags {
     volatile int32_t needsReset     __attribute__ ((aligned (4)));
 } BDSKZoomGroupFlags;    
 
+
+@class BDSKThreadSafeMutableDictionary;
+
 @interface BDSKZoomGroupServer : BDSKAsynchronousDOServer <BDSKSearchGroupServer, BDSKZoomGroupServerMainThread, BDSKZoomGroupServerLocalThread>
 {
     BDSKSearchGroup *group;
     BDSKZoomConnection *connection;
-    NSString *host;
-    int port;
-    NSString *user;
-    NSString *password;
-    NSString *database;
+    BDSKThreadSafeMutableDictionary *serverInfo;
     int availableResults;
     int fetchedResults;
     BDSKZoomGroupFlags flags;
-    pthread_rwlock_t infolock;
 }
 - (void)resetConnection;
 @end
