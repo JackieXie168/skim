65c227f5dd0b5094585932f11c77c191088a5662
mmcc <mmcc@gmail.com>
2007-01-05 22:45:25 +0000

    Add file to store string constants and an initial user defaults config file.

---
+++ SKStringConstants.h
@@ -0,0 +1,9 @@
+//
+//  SKStringConstants.h
+//  Skim
+//  This code is licensed under a BSD license. Please see the file LICENSE for details.
+//
+//  Created by Michael McCracken on 1/5/07.
+
+
+extern NSString *SKMainWindowFrameAutosaveName;
---
+++ SKStringConstants.m
@@ -0,0 +1,10 @@
+//
+//  SKStringConstants.m
+//  Skim
+//  This code is licensed under a BSD license. Please see the file LICENSE for details.
+//
+//  Created by Michael McCracken on 1/5/07.
+
+#import "SKStringConstants.h"
+
+NSString *SKMainWindowFrameAutosaveName = @"SKIM: main window frame autosave";
