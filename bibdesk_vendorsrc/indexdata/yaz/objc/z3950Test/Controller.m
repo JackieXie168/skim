74a1fb6b1e9150281e0f9180ded1f6c34e1b5fd0
Adam Maxwell <amaxwell@lists.sourceforge.net>
2007-01-06 23:00:14 +0000

    update test project and remove some unused code

--- bibdesk_vendorsrc/indexdata/yaz/objc/z3950Test/Controller.m
+++ bibdesk_vendorsrc/indexdata/yaz/objc/z3950Test/Controller.m
@@ -11,9 +11,8 @@
 
 - (void)awakeFromNib
 {
-    //_connection = [[BDSKZoomConnection alloc] initWithHost:@"z3950.loc.gov:7090/Voyager" port:0];
     
-    
+    /* Should create plists for these, but I don't feel like it */
     
     /* standard server */
     /*
@@ -53,16 +52,16 @@
     [_dbaseField setStringValue:_database];
     [_portField setIntValue:_port];
     
-    [BDSKZoomRecord setFallbackEncoding:NSWindowsCP1252StringEncoding];
+    [ZOOMRecord setFallbackEncoding:NSISOLatin1StringEncoding];
     
     [_popup removeAllItems];
-    [_popup addItemsWithTitles:[BDSKZoomRecord validKeys]];
+    [_popup addItemsWithTitles:[ZOOMRecord validKeys]];
     [_popup selectItemAtIndex:0];
-    _currentType = [[[BDSKZoomRecord validKeys] objectAtIndex:0] copy];
+    _currentType = [[[ZOOMRecord validKeys] objectAtIndex:0] copy];
     _syntaxType = USMARC;
     
     [_searchField setDelegate:self];
-    [_searchField setFormatter:[[[BDSKZoomCCLQueryFormatter alloc] init] autorelease]];
+    [_searchField setFormatter:[[[ZOOMCCLQueryFormatter alloc] init] autorelease]];
     
     [_syntaxPopup removeAllItems];
     [_syntaxPopup addItemsWithTitles:[NSArray arrayWithObjects:@"USMARC", @"GRS-1", @"SUTRS", @"XML", @"UKMARC", nil]];
@@ -95,7 +94,7 @@
 
 - (IBAction)changeSyntaxType:(id)sender;
 {
-    _syntaxType = [BDSKZoomRecord syntaxTypeWithString:[sender titleOfSelectedItem]];
+    _syntaxType = [ZOOMRecord syntaxTypeWithString:[sender titleOfSelectedItem]];
     [_connection setPreferredRecordSyntax:_syntaxType];
     [self search:_searchField];
     NSLog(@"%@", [_connection propertyList]);
@@ -104,7 +103,7 @@
 - (void)makeNewConnection
 {
     [_connection release];
-    _connection = [[BDSKZoomConnection alloc] initWithHost:_hostname port:_port database:_database];
+    _connection = [[ZOOMConnection alloc] initWithHost:_hostname port:_port database:_database];
     [_connection setPreferredRecordSyntax:_syntaxType];
     _connectionNeedsReset = NO;
     NSEnumerator *keyE = [_options keyEnumerator];
@@ -132,11 +131,6 @@
     [_database release];
     _database = [[sender stringValue] copy];
     _connectionNeedsReset = YES;
-}
-
-- (void)applicationWillTerminate:(NSNotification *)notification;
-{
-    ZOOM_connection_destroy(connection);
 }
 
 - (BOOL)control:(NSControl *)control didFailToFormatString:(NSString *)string errorDescription:(NSString *)error
@@ -193,8 +187,6 @@ static NSString *joinedArrayComponents(NSArray *arrayOfXMLNodes)
     return arrayOfPubs;
 }
     
-    
-
 - (IBAction)search:(id)sender;
 {
     NSString *searchString = [sender stringValue];
@@ -206,7 +198,7 @@ static NSString *joinedArrayComponents(NSArray *arrayOfXMLNodes)
         [_textView setString:@""];
     } else {
         
-        BDSKZoomResultSet *resultSet = [_connection resultsForCCLQuery:searchString];
+        ZOOMResultSet *resultSet = [_connection resultsForCCLQuery:searchString];
         
         unsigned int count = [resultSet countOfRecords];
         [_textView setString:[NSString stringWithFormat:@"%d results found for \"%@\"", count, searchString]];
@@ -214,15 +206,15 @@ static NSString *joinedArrayComponents(NSArray *arrayOfXMLNodes)
         if (count) {
             unsigned i, iMax = MIN(5, count);
             NSMutableString *mutableString = [[_textView textStorage] mutableString];
-            BDSKZoomRecord *record;
+            ZOOMRecord *record;
 
             for (i = 0; i < iMax; i++) {
                 [mutableString appendString:@"\n\n"];
                 [mutableString appendFormat:@"***** RECORD %d *****\n", i];
                 record = [resultSet recordAtIndex:i];
-                [mutableString appendFormat:@"Syntax: %@\n", [BDSKZoomRecord stringWithSyntaxType:[record syntaxType]]];
+                [mutableString appendFormat:@"Syntax: %@\n", [ZOOMRecord stringWithSyntaxType:[record syntaxType]]];
                 
-                NSString *value = [record valueForKey:_currentType];
+                NSString *value = [record stringValueForKey:_currentType];
                 [mutableString appendString:(value ? value : [NSString stringWithFormat:@"record returned nil for %@", _currentType])];
                 NSLog(@"%@", [self dictionariesWithXMLString:value]);
             }
@@ -235,49 +227,6 @@ static NSString *joinedArrayComponents(NSArray *arrayOfXMLNodes)
     [_currentType release];
     _currentType = [[sender titleOfSelectedItem] copy];
     [self search:_searchField];
-}
-
-- (void)doNothing:(id)sender
-{
-    ZOOM_resultset r;
-    const char *rec;
-    
-    NSString *searchString = [sender stringValue];
-    // r = ZOOM_connection_search_pqf(connection, [[sender stringValue] UTF8String]);
-    
-    if (nil == searchString || [searchString isEqualToString:@""]) {
-        [_textView setString:@""];
-    } else {
-        ZOOM_query query = ZOOM_query_create();
-        const char *errstring;
-        int error, errorPosition;
-        if (ZOOM_query_ccl2rpn(query, [searchString UTF8String], "term t=l,r s=al\n" "ti u=4 s=pw\n", &error, &errstring, &errorPosition) == 0) {
-            r = ZOOM_connection_search(connection, query);
-            
-            [_textView setString:[NSString stringWithFormat:@"%d results found for \"%@\"", ZOOM_resultset_size(r), searchString]];
-            [[[_textView textStorage] mutableString] appendString:@"\n\n"];
-
-            ZOOM_record zrecord = ZOOM_resultset_record(r, 0);
-            
-            // string "ventin" produces error
-            rec = ZOOM_record_get(zrecord, "render", NULL);
-            NSString *record = (rec ? [NSString stringWithCString:rec encoding:NSUTF8StringEncoding] : @"Nothing found");
-            
-            if (nil == record) {
-                record = @"Unable to convert c string to NSString";
-                record = [record stringByAppendingFormat:@"\n\n%@", [NSString stringWithCString:rec encoding:NSISOLatin1StringEncoding]];
-            }
-            
-            if ([@"" isEqualToString:record])
-                record = @"Empty description of record";
-            
-            [[[_textView textStorage] mutableString] appendString:record];
-        } else {
-            [_textView setString:@"Unable to translate ccl query"];
-        }
-        ZOOM_query_destroy(query);
-        [[_textView textStorage] addAttribute:NSFontAttributeName value:[NSFont userFixedPitchFontOfSize:10.0] range:NSMakeRange(0, [[_textView textStorage] length])];
-    }
 }
 
 @end
