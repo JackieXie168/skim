5e4deeee5119b36fe7ca66c86c916c41c64a7343
amaxwell <amaxwell@gmail.com>
2007-01-28 21:36:34 +0000

    Add a separate object for collecting paths, instead of keeping ivars for each one.  Remove aux file when deleting files before a new run.

--- bibdesk/BDSKTeXTask.m
+++ bibdesk/BDSKTeXTask.m
@@ -46,6 +46,23 @@
 #import "BDSKShellCommandFormatter.h"
 #import <libkern/OSAtomic.h>
 
+@interface BDSKTeXPath : NSObject
+{
+    NSString *fullPathWithoutExtension;
+}
+- (id)initWithBasePath:(NSString *)fullPath;
+- (NSString *)baseNameWithoutExtension;
+- (NSString *)workingDirectory;
+- (NSString *)texFilePath;
+- (NSString *)bibFilePath;
+- (NSString *)bblFilePath;
+- (NSString *)pdfFilePath;
+- (NSString *)rtfFilePath;
+- (NSString *)logFilePath;
+- (NSString *)blgFilePath;
+- (NSString *)auxFilePath;
+@end
+
 @interface BDSKTeXTask (Private) 
 
 - (void)writeHelperFiles;
@@ -90,23 +107,13 @@
 		
 		NSFileManager *fm = [NSFileManager defaultManager];
         
-		workingDirPath = [dirPath retain];
-		if (![fm objectExistsAtFileURL:[NSURL fileURLWithPath:workingDirPath]])
-			[fm createDirectoryAtPathWithNoAttributes:workingDirPath];
+		if (![fm objectExistsAtFileURL:[NSURL fileURLWithPath:dirPath]])
+			[fm createDirectoryAtPathWithNoAttributes:dirPath];
 		
-		applicationSupportPath = [[fm currentApplicationSupportPathForCurrentUser] retain];
-		texTemplatePath = [[applicationSupportPath stringByAppendingPathComponent:@"previewtemplate.tex"] retain];
-		
-		fileName = [newFileName retain];
-		
-		NSString *filePath = [workingDirPath stringByAppendingPathComponent:newFileName];
-		texFilePath = [[filePath stringByAppendingPathExtension:@"tex"] retain];
-		bibFilePath = [[filePath stringByAppendingPathExtension:@"bib"] retain];
-        bblFilePath = [[filePath stringByAppendingPathExtension:@"bbl"] retain];
-        pdfFilePath = [[filePath stringByAppendingPathExtension:@"pdf"] retain];
-        rtfFilePath = [[filePath stringByAppendingPathExtension:@"rtf"] retain];
-        logFilePath = [[filePath stringByAppendingPathExtension:@"log"] retain];
-        blgFilePath = [[filePath stringByAppendingPathExtension:@"blg"] retain];
+		texTemplatePath = [[[fm currentApplicationSupportPathForCurrentUser] stringByAppendingPathComponent:@"previewtemplate.tex"] copy];
+				
+		NSString *filePath = [dirPath stringByAppendingPathComponent:newFileName];
+        texPath = [[BDSKTeXPath alloc] initWithBasePath:filePath];
         
 		binDirPath = nil; // set from where we run the tasks, since some programs (e.g. XeLaTeX) need a real path setting     
         
@@ -114,8 +121,8 @@
         const char *bibInputs = getenv("BIBINPUTS");
         if(bibInputs != NULL){
             NSString *value = [NSString stringWithCString:bibInputs];
-            if([value rangeOfString:workingDirPath].length == 0){
-                value = [NSString stringWithFormat:@"%@:%@", value, workingDirPath];
+            if([value rangeOfString:[texPath workingDirectory]].length == 0){
+                value = [NSString stringWithFormat:@"%@:%@", value, [texPath workingDirectory]];
                 setenv("BIBINPUTS", [value cString], 1);
             }
         }        
@@ -134,17 +141,8 @@
 }
 
 - (void)dealloc{
-	[workingDirPath release];
-    [applicationSupportPath release];
     [texTemplatePath release];
-    [fileName release];
-    [texFilePath release];
-    [bibFilePath release];
-    [bblFilePath release];
-    [pdfFilePath release];
-    [rtfFilePath release];
-    [logFilePath release];
-    [blgFilePath release];
+    [texPath release];
     [taskShouldStartInvocation release];
     [taskFinishedInvocation release];
     OFSimpleLockFree(&processingLock);
@@ -154,7 +152,7 @@
 
 - (NSString *)description{
     NSMutableString *temporaryDescription = [[NSMutableString alloc] initWithString:[super description]];
-    [temporaryDescription appendFormat:@" {\nivars:\n\tdelegate = \"%@\"\n\tfile name = \"%@\"\n\ttemplate = \"%@\"\n\tTeX file = \"%@\"\n\tBibTeX file = \"%@\"\n\tTeX binary path = \"%@\"\n\tEncoding = \"%@\"\n\tBibTeX style = \"%@\"\n\nenvironment:\n\tSHELL = \"%s\"\n\tBIBINPUTS = \"%s\"\n\tBSTINPUTS = \"%s\"\n\tPATH = \"%s\" }", delegate, fileName, texTemplatePath, texFilePath, bibFilePath, binDirPath, [NSString localizedNameOfStringEncoding:[[OFPreferenceWrapper sharedPreferenceWrapper] integerForKey:BDSKTeXPreviewFileEncodingKey]], [[OFPreferenceWrapper sharedPreferenceWrapper] objectForKey:BDSKBTStyleKey], getenv("SHELL"), getenv("BIBINPUTS"), getenv("BSTINPUTS"), getenv("PATH")];
+    [temporaryDescription appendFormat:@" {\nivars:\n\tdelegate = \"%@\"\n\tfile name = \"%@\"\n\ttemplate = \"%@\"\n\tTeX file = \"%@\"\n\tBibTeX file = \"%@\"\n\tTeX binary path = \"%@\"\n\tEncoding = \"%@\"\n\tBibTeX style = \"%@\"\n\nenvironment:\n\tSHELL = \"%s\"\n\tBIBINPUTS = \"%s\"\n\tBSTINPUTS = \"%s\"\n\tPATH = \"%s\" }", delegate, [texPath baseNameWithoutExtension], texTemplatePath, [texPath texFilePath], [texPath bibFilePath], binDirPath, [NSString localizedNameOfStringEncoding:[[OFPreferenceWrapper sharedPreferenceWrapper] integerForKey:BDSKTeXPreviewFileEncodingKey]], [[OFPreferenceWrapper sharedPreferenceWrapper] objectForKey:BDSKBTStyleKey], getenv("SHELL"), getenv("BIBINPUTS"), getenv("BSTINPUTS"), getenv("PATH")];
     NSString *description = [temporaryDescription copy];
     [temporaryDescription release];
     return [description autorelease];
@@ -304,17 +302,17 @@
     if(0 == pthread_rwlock_tryrdlock(&dataFileLock)) {
         // @@ unclear if log files will always be written with ASCII encoding
         // these will be nil if the file doesn't exist
-        logString = [NSString stringWithContentsOfFile:logFilePath encoding:NSASCIIStringEncoding error:NULL];
-        blgString = [NSString stringWithContentsOfFile:blgFilePath encoding:NSASCIIStringEncoding error:NULL];
+        logString = [NSString stringWithContentsOfFile:[texPath logFilePath] encoding:NSASCIIStringEncoding error:NULL];
+        blgString = [NSString stringWithContentsOfFile:[texPath blgFilePath] encoding:NSASCIIStringEncoding error:NULL];
         pthread_rwlock_unlock(&dataFileLock);
     }
     
     NSMutableString *toReturn = [NSMutableString string];
     [toReturn setString:@"---------- TeX log file ----------\n"];
-    [toReturn appendFormat:@"File: \"%@\"\n", logFilePath];
+    [toReturn appendFormat:@"File: \"%@\"\n", [texPath logFilePath]];
     [toReturn appendFormat:@"%@\n\n", logString];
     [toReturn appendString:@"---------- BibTeX log file -------\n"];
-    [toReturn appendFormat:@"File: \"%@\"\n", blgFilePath];
+    [toReturn appendFormat:@"File: \"%@\"\n", [texPath blgFilePath]];
     [toReturn appendFormat:@"%@\n\n", blgString];
     [toReturn appendString:@"---------- BibDesk info ----------\n"];
     [toReturn appendString:[self description]];
@@ -326,7 +324,7 @@
 - (NSString *)LTBString{
     NSString *string = nil;
     if([self hasLTB] && 0 == pthread_rwlock_tryrdlock(&dataFileLock)) {
-        string = [NSString stringWithContentsOfFile:bblFilePath encoding:[[OFPreferenceWrapper sharedPreferenceWrapper] integerForKey:BDSKTeXPreviewFileEncodingKey] error:NULL];
+        string = [NSString stringWithContentsOfFile:[texPath bblFilePath] encoding:[[OFPreferenceWrapper sharedPreferenceWrapper] integerForKey:BDSKTeXPreviewFileEncodingKey] error:NULL];
         pthread_rwlock_unlock(&dataFileLock);
         unsigned start, end;
         start = [string rangeOfString:@"\\bib{"].location;
@@ -340,7 +338,7 @@
 - (NSString *)LaTeXString{
     NSString *string = nil;
     if([self hasLaTeX] && 0 == pthread_rwlock_tryrdlock(&dataFileLock)) {
-        string = [NSString stringWithContentsOfFile:bblFilePath encoding:[[OFPreferenceWrapper sharedPreferenceWrapper] integerForKey:BDSKTeXPreviewFileEncodingKey] error:NULL];
+        string = [NSString stringWithContentsOfFile:[texPath bblFilePath] encoding:[[OFPreferenceWrapper sharedPreferenceWrapper] integerForKey:BDSKTeXPreviewFileEncodingKey] error:NULL];
         pthread_rwlock_unlock(&dataFileLock);
         unsigned start, end;
         start = [string rangeOfString:@"\\bibitem"].location;
@@ -354,7 +352,7 @@
 - (NSData *)PDFData{
     NSData *data = nil;
     if ([self hasPDFData] && 0 == pthread_rwlock_tryrdlock(&dataFileLock)) {
-        data = [NSData dataWithContentsOfFile:pdfFilePath];
+        data = [NSData dataWithContentsOfFile:[texPath pdfFilePath]];
         pthread_rwlock_unlock(&dataFileLock);
     }
     return data;
@@ -363,30 +361,30 @@
 - (NSData *)RTFData{
     NSData *data = nil;
     if ([self hasRTFData] && 0 == pthread_rwlock_tryrdlock(&dataFileLock)) {
-        data = [NSData dataWithContentsOfFile:rtfFilePath];
+        data = [NSData dataWithContentsOfFile:[texPath rtfFilePath]];
         pthread_rwlock_unlock(&dataFileLock);
     }
     return data;
 }
 
 - (NSString *)logFilePath{
-    return logFilePath;
+    return [texPath logFilePath];
 }
 
 - (NSString *)LTBFilePath{
-    return [self hasLTB] ? bblFilePath : nil;
+    return [self hasLTB] ? [texPath bblFilePath] : nil;
 }
 
 - (NSString *)LaTeXFilePath{
-    return [self hasLaTeX] ? bblFilePath : nil;
+    return [self hasLaTeX] ? [texPath bblFilePath] : nil;
 }
 
 - (NSString *)PDFFilePath{
-    return [self hasPDFData] ? pdfFilePath : nil;
+    return [self hasPDFData] ? [texPath pdfFilePath] : nil;
 }
 
 - (NSString *)RTFFilePath{
-    return [self hasRTFData] ? rtfFilePath : nil;
+    return [self hasRTFData] ? [texPath rtfFilePath] : nil;
 }
 
 - (BOOL)hasLTB{
@@ -420,13 +418,13 @@
 @implementation BDSKTeXTask (Private)
 
 - (void)writeHelperFiles{
-    UKDirectoryEnumerator *enumerator = [UKDirectoryEnumerator enumeratorWithPath:applicationSupportPath];
+    UKDirectoryEnumerator *enumerator = [UKDirectoryEnumerator enumeratorWithPath:[[NSFileManager defaultManager] currentApplicationSupportPathForCurrentUser]];
     [enumerator setDesiredInfo:kFSCatInfoNodeFlags];
     
 	NSString *path = nil;
     NSString *pathExt = nil;
     
-    NSURL *dstURL = [NSURL fileURLWithPath:workingDirPath];
+    NSURL *dstURL = [NSURL fileURLWithPath:[texPath workingDirectory]];
     NSError *error;
 		
 	// copy all user .cfg and .sty files from application support
@@ -437,7 +435,7 @@
 		    [pathExt isEqual:@"sty"])){
 			
 			if(![[NSFileManager defaultManager] copyObjectAtURL:[NSURL fileURLWithPath:path] toDirectoryAtURL:dstURL error:&error])
-                NSLog(@"unable to copy helper file %@ to %@; error %@", path, workingDirPath, [error localizedDescription]);
+                NSLog(@"unable to copy helper file %@ to %@; error %@", path, [texPath workingDirectory], [error localizedDescription]);
 		}
 	}
 }
@@ -458,11 +456,11 @@
     
     if (nil != texFile) {
 	
-        [texFile replaceOccurrencesOfString:@"<<File>>" withString:fileName options:NSCaseInsensitiveSearch range:NSMakeRange(0,[texFile length])];
+        [texFile replaceOccurrencesOfString:@"<<File>>" withString:[texPath baseNameWithoutExtension] options:NSCaseInsensitiveSearch range:NSMakeRange(0,[texFile length])];
         [texFile replaceOccurrencesOfString:@"<<Style>>" withString:style options:NSCaseInsensitiveSearch range:NSMakeRange(0,[texFile length])];
 
         // overwrites the old tmpbib.tex file, replacing the previous bibliographystyle
-        didWrite = [[texFile dataUsingEncoding:encoding] writeToFile:texFilePath atomically:YES];
+        didWrite = [[texFile dataUsingEncoding:encoding] writeToFile:[texPath texFilePath] atomically:YES];
         if(NO == didWrite)
             NSLog(@"error writing TeX file with encoding %@ for task %@", [NSString localizedNameOfStringEncoding:encoding], self);
 	
@@ -495,7 +493,7 @@
     [bibTemplate appendString:@"\n"];
         
     BOOL didWrite;
-    didWrite = [bibTemplate writeToFile:bibFilePath atomically:NO encoding:encoding error:&error];
+    didWrite = [bibTemplate writeToFile:[texPath bibFilePath] atomically:NO encoding:encoding error:&error];
     if(NO == didWrite) {
         NSLog(@"error writing BibTeX file with encoding %@ for task %@", [NSString localizedNameOfStringEncoding:encoding], self);
         NSLog(@"Foundation reported error %@", error);
@@ -506,10 +504,10 @@
 }
 
 // caller must have acquired wrlock on dataFileLock
-- (void)removeLogFilesFromPreviousRun{
+- (void)removeFilesFromPreviousRun{
     // use FSDeleteObject for thread safety
     const FSRef fileRef;
-    NSArray *filesToRemove = [NSArray arrayWithObjects:blgFilePath, logFilePath, bblFilePath, nil];
+    NSArray *filesToRemove = [NSArray arrayWithObjects:[texPath blgFilePath], [texPath logFilePath], [texPath bblFilePath], [texPath auxFilePath], nil];
     NSEnumerator *e = [filesToRemove objectEnumerator];
     NSString *path;
     CFAllocatorRef alloc = CFRetain(CFAllocatorGetDefault());
@@ -538,7 +536,7 @@
     }
 
     // nuke the log files in case the run fails without generating new ones (not very likely)
-    [self removeLogFilesFromPreviousRun];
+    [self removeFilesFromPreviousRun];
         
     if(![self runPDFTeXTask] ||
        ![self runBibTeXTask]){
@@ -610,7 +608,7 @@
     NSString *pdftexbinpath = [BDSKShellCommandFormatter pathByRemovingArgumentsFromCommand:command];
     NSMutableArray *args = [NSMutableArray arrayWithObject:@"-interaction=batchmode"];
     [args addObjectsFromArray:argArray];
-    [args addObject:fileName];
+    [args addObject:[texPath baseNameWithoutExtension]];
     
     // This task runs latex on our tex file 
     return [self runTask:pdftexbinpath withArguments:args];
@@ -623,7 +621,7 @@
     NSString *bibtexbinpath = [BDSKShellCommandFormatter pathByRemovingArgumentsFromCommand:command];
     NSMutableArray *args = [NSMutableArray array];
     [args addObjectsFromArray:argArray];
-    [args addObject:fileName];
+    [args addObject:[texPath baseNameWithoutExtension]];
     
     // This task runs bibtex on our bib file 
     return [self runTask:bibtexbinpath withArguments:args];
@@ -634,12 +632,12 @@
     
     // This task runs latex2rtf on our tex file to generate tmpbib.rtf
     // the arguments: it needs -P "path" which is the path to the cfg files in the app wrapper
-    return [self runTask:latex2rtfpath withArguments:[NSArray arrayWithObjects:@"-P", [[NSBundle mainBundle] resourcePath], fileName, nil]];
+    return [self runTask:latex2rtfpath withArguments:[NSArray arrayWithObjects:@"-P", [[NSBundle mainBundle] resourcePath], [texPath baseNameWithoutExtension], nil]];
 }
 
 - (BOOL)runTask:(NSString *)binPath withArguments:(NSArray *)arguments{
     currentTask = [[NSTask alloc] init];
-    [currentTask setCurrentDirectoryPath:workingDirPath];
+    [currentTask setCurrentDirectoryPath:[texPath workingDirectory]];
     [currentTask setLaunchPath:binPath];
     [currentTask setArguments:arguments];
     [currentTask setStandardOutput:[NSFileHandle fileHandleWithNullDevice]];
@@ -678,5 +676,37 @@
 
     return success;
 }
+
+@end
+
+@implementation BDSKTeXPath
+
+- (id)initWithBasePath:(NSString *)fullPath;
+{
+    self = [super init];
+    if (self) {
+        // this gives e.g. /tmp/preview/bibpreview, where bibpreview is the basename of all files, and /tmp/preview is the working directory
+        fullPathWithoutExtension = [[fullPath stringByStandardizingPath] copy];
+        NSParameterAssert(fullPathWithoutExtension);
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [fullPathWithoutExtension release];
+    [super dealloc];
+}
+
+- (NSString *)baseNameWithoutExtension { return [fullPathWithoutExtension lastPathComponent]; }
+- (NSString *)workingDirectory { return [fullPathWithoutExtension stringByDeletingLastPathComponent]; }
+- (NSString *)texFilePath { return [fullPathWithoutExtension stringByAppendingPathExtension:@"tex"]; }
+- (NSString *)bibFilePath { return [fullPathWithoutExtension stringByAppendingPathExtension:@"bib"]; }
+- (NSString *)bblFilePath { return [fullPathWithoutExtension stringByAppendingPathExtension:@"bbl"]; }
+- (NSString *)pdfFilePath { return [fullPathWithoutExtension stringByAppendingPathExtension:@"pdf"]; }
+- (NSString *)rtfFilePath { return [fullPathWithoutExtension stringByAppendingPathExtension:@"rtf"]; }
+- (NSString *)logFilePath { return [fullPathWithoutExtension stringByAppendingPathExtension:@"log"]; }
+- (NSString *)blgFilePath { return [fullPathWithoutExtension stringByAppendingPathExtension:@"blg"]; }
+- (NSString *)auxFilePath { return [fullPathWithoutExtension stringByAppendingPathExtension:@"aux"]; }
 
 @end
