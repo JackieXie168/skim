441282130456c89657eaad44f9bb898e057a3eb6
Adam Maxwell <amaxwell@lists.sourceforge.net>
2007-01-31 02:23:15 +0000

    missed header on last commit

--- bibdesk/NSError_BDSKExtensions.h
+++ bibdesk/NSError_BDSKExtensions.h
@@ -52,6 +52,7 @@ enum {
     kBDSKNetworkConnectionFailed,                /* Unable to connect to a network          */
     kBDSKFileNotFound,                           /* File not found (should have URL/path)   */
     kBDSKAppleScriptError,                       /* AppleScript failed                      */
+    kBDSKParserIgnoredFrontMatter,               /* BibTeXParser ignored front matter       */
 };
 
 extern NSString *BDSKUnderlyingItemErrorKey;
