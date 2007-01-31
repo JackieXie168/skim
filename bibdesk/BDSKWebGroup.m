f31c16608e3609dbda86fda96362f626d27624a0
hofman <hofman@gmail.com>
2007-01-31 14:08:54 +0000

    Show spinner for web group while a frame is loading.

--- bibdesk/BDSKWebGroup.m
+++ bibdesk/BDSKWebGroup.m
@@ -20,7 +20,7 @@
     if (self = [super initWithName:aName count:0]) {
         publications = nil;
         macroResolver = [[BDSKMacroResolver alloc] initWithOwner:self];
-
+        isRetrieving = NO;
     }
     return self;
 }
@@ -66,7 +66,9 @@
 
 - (BOOL)hasEditableName { return NO; }
 
-- (BOOL)isRetrieving { return NO;}
+- (BOOL)isRetrieving { return isRetrieving; }
+
+- (void)setRetrieving:(BOOL)flag { isRetrieving = flag; }
 
 - (BOOL)failedDownload { return NO;}
 
