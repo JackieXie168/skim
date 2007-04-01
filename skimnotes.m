2fe1b4a236e5f39d91495668229a09411d4715c0
hofman <hofman@gmail.com>
2007-04-01 14:59:06 +0000

    Complete relative paths. Derive notes file path from pdf file path when not provided.

--- skimnotes.m
+++ skimnotes.m
@@ -3,7 +3,7 @@
 
 #define SKIM_NOTES_KEY @"net_sourceforge_skim-app_notes"
 
-static char *usageStr = "Usage:\n skimnotes set PDF_FILE SKIM_FILE\n skimnotes get PDF_FILE SKIM_FILE\n skimnotes remove PDF_FILE";
+static char *usageStr = "Usage:\n skimnotes set PDF_FILE [SKIM_FILE]\n skimnotes get PDF_FILE [SKIM_FILE]\n skimnotes remove PDF_FILE";
 static char *versionStr = "SkimNotes command-line client, version 0.1.";
 
 enum {
@@ -12,15 +12,22 @@ enum {
     SKNActionRemove
 };
 
+static inline NSString *SKNNormalizedPath(NSString *path, NSString *basePath) {
+    unichar ch = [path length] ? [path characterAtIndex:0] : 0;
+    if (basePath && ch != '/' && ch != '~')
+        path = [basePath stringByAppendingPathComponent:path];
+    return [path stringByStandardizingPath];
+}
+
 int main (int argc, const char * argv[]) {
 	int action = 0;
     
     if (argc == 2 && (strcmp("-h", argv[1]) == 0 || strcmp("-help", argv[1]) == 0)) {
         fprintf (stderr, "%s\n%s\n", usageStr, versionStr);
         exit (0);
-    } else if (argc > 3 && strcmp("get", argv[1]) == 0) {
+    } else if (argc > 2 && strcmp("get", argv[1]) == 0) {
         action = SKNActionGet;
-    } else if (argc > 3 && strcmp("set", argv[1]) == 0) {
+    } else if (argc > 2 && strcmp("set", argv[1]) == 0) {
         action = SKNActionSet;
     } else if (argc > 2 && strcmp("remove", argv[1]) == 0) {
         action = SKNActionRemove;
@@ -29,13 +36,25 @@ int main (int argc, const char * argv[]) {
         exit (1);
     }
     
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
  
     NSFileManager *fm = [NSFileManager defaultManager];
     BOOL success = NO;
-    NSString *pdfPath = [[[[NSProcessInfo processInfo] arguments] objectAtIndex:2] stringByStandardizingPath];
-    NSString *notesPath = action == SKNActionRemove ? nil : [[[[NSProcessInfo processInfo] arguments] objectAtIndex:3] stringByStandardizingPath];
+    NSString *currentDir = [fm currentDirectoryPath];
+    NSString *pdfPath = SKNNormalizedPath([[[NSProcessInfo processInfo] arguments] objectAtIndex:2], currentDir);
+    NSString *notesPath = argc < 4 ? nil : SKNNormalizedPath([[[NSProcessInfo processInfo] arguments] objectAtIndex:3], currentDir);
     BOOL isDir = NO;
+    
+    if (action != SKNActionRemove && notesPath == nil) {
+        if ([[pdfPath pathExtension] caseInsensitiveCompare:@"pdf"] == NSOrderedSame)
+            notesPath = [[pdfPath stringByDeletingPathExtension] stringByAppendingPathExtension:@"skim"];
+        else
+            notesPath = [pdfPath stringByAppendingPathExtension:@"skim"];
+    }
+    
+    if ([[pdfPath pathExtension] caseInsensitiveCompare:@"pdf"] == NSOrderedSame && 
+        ([fm fileExistsAtPath:pdfPath isDirectory:&isDir] == NO || isDir))
+        pdfPath = [pdfPath stringByAppendingPathExtension:@"pdf"];
     
     if ([fm fileExistsAtPath:pdfPath isDirectory:&isDir] == NO || isDir) {
     } else if (action == SKNActionGet) {
