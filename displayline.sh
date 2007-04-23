a69429fabe0c7b68cc151a2389b265165da6f651
Christiaan Hofman <hofman@lists.sourceforge.net>
2007-04-23 10:21:54 +0000

    Use correct arguments to pass to applescript for displaying tex line.

--- displayline.sh
+++ displayline.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# displayline.sh (Skim)
+#
+# Usage: displayline.sh LINE PDFFILE [TEXSOURCEFILE]
+
 line=$1
 file="$2"
-project="$2"
 if [ $# -gt 3 ]; then
-project="$2"
+source="$3"
+else
+source="$2"
 fi
 
-/usr/bin/osascript -e "tell application \"Skim\"" -e "activate" -e "display TeX line "${line}" in \""${file}"\" from source \""${project}"\"" -e "end tell"
+/usr/bin/osascript -e "tell application \"Skim\"" -e "activate" -e "display TeX line "${line}" in \""${file}"\" from source \""${source}"\"" -e "end tell"
