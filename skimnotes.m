08c9471a225149c3a6a313516d76c8cdf2664976
hofman <hofman@gmail.com>
2007-05-25 20:40:03 +0000

    Support for rtf and text notes in skimnotes tool.

--- skimnotes.m
+++ skimnotes.m
@@ -2,9 +2,11 @@
 #import "NSFileManager_ExtendedAttributes.h"
 
 #define SKIM_NOTES_KEY @"net_sourceforge_skim-app_notes"
+#define SKIM_RTF_NOTES_KEY @"net_sourceforge_skim-app_rtf_notes"
+#define SKIM_TEXT_NOTES_KEY @"net_sourceforge_skim-app_text_notes"
 
-static char *usageStr = "Usage:\n skimnotes set PDF_FILE [SKIM_FILE]\n skimnotes get PDF_FILE [SKIM_FILE]\n skimnotes remove PDF_FILE";
-static char *versionStr = "SkimNotes command-line client, version 0.1.";
+static char *usageStr = "Usage:\n skimnotes set PDF_FILE [SKIM_FILE]\n skimnotes get PDF_FILE [SKIM_FILE|RTF_FILE|TEXT_FILE]\n skimnotes remove PDF_FILE";
+static char *versionStr = "SkimNotes command-line client, version 0.2.";
 
 enum {
     SKNActionGet,
@@ -59,9 +61,22 @@ int main (int argc, const char * argv[]) {
     if ([fm fileExistsAtPath:pdfPath isDirectory:&isDir] == NO || isDir) {
     } else if (action == SKNActionGet) {
         NSError *error = nil;
-        NSData *data = [fm extendedAttributeNamed:SKIM_NOTES_KEY atPath:pdfPath traverseLink:YES error:&error];
-        if (data == nil && [error code] == ENOATTR)
-            data = [NSKeyedArchiver archivedDataWithRootObject:[NSArray array]];
+        NSData *data = nil;
+        if ([[notesPath pathExtension] caseInsensitiveCompare:@"rtf"] == NSOrderedSame) {
+            data = [fm extendedAttributeNamed:SKIM_RTF_NOTES_KEY atPath:pdfPath traverseLink:YES error:&error];
+            if (data == nil && [error code] == ENOATTR)
+                data = [NSData data];
+        } else if ([[notesPath pathExtension] caseInsensitiveCompare:@"txt"] == NSOrderedSame || [[notesPath pathExtension] caseInsensitiveCompare:@"text"] == NSOrderedSame) {
+            NSError *error = nil;
+            data = [fm extendedAttributeNamed:SKIM_TEXT_NOTES_KEY atPath:pdfPath traverseLink:YES error:&error];
+            if (data == nil && [error code] == ENOATTR)
+                data = [NSData data];
+        } else {
+            NSError *error = nil;
+            data = [fm extendedAttributeNamed:SKIM_NOTES_KEY atPath:pdfPath traverseLink:YES error:&error];
+            if (data == nil && [error code] == ENOATTR)
+                data = [NSKeyedArchiver archivedDataWithRootObject:[NSArray array]];
+        }
         if (data)
             success = [data writeToFile:notesPath atomically:YES];
     } else if (action == SKNActionSet && notesPath && [fm fileExistsAtPath:notesPath isDirectory:&isDir] && isDir == NO) {
@@ -74,9 +89,16 @@ int main (int argc, const char * argv[]) {
         }
     } else if (action == SKNActionRemove) {
         NSError *error = nil;
-        success = [fm removeExtendedAttribute:SKIM_NOTES_KEY atPath:pdfPath traverseLink:YES error:&error];
-        if (success == NO && [error code] == ENOATTR)
-            success = YES;
+        BOOL success1 = [fm removeExtendedAttribute:SKIM_NOTES_KEY atPath:pdfPath traverseLink:YES error:&error];
+        if (success1 == NO && [error code] == ENOATTR)
+            success1 = YES;
+        BOOL success2 = [fm removeExtendedAttribute:SKIM_RTF_NOTES_KEY atPath:pdfPath traverseLink:YES error:&error];
+        if (success2 == NO && [error code] == ENOATTR)
+            success2 = YES;
+        BOOL success3 = [fm removeExtendedAttribute:SKIM_TEXT_NOTES_KEY atPath:pdfPath traverseLink:YES error:&error];
+        if (success3 == NO && [error code] == ENOATTR)
+            success3 = YES;
+        success = success1 && success2 && success3;
     }
     
     [pool release];
