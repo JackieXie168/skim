dd91ddc5116b0bb906e9d365021feda059a70adf
hofman <hofman@gmail.com>
2007-06-17 16:42:18 +0000

    Move NSColor category to separate files.

---
+++ NSColor_SKExtensions.m
@@ -0,0 +1,112 @@
+//
+//  NSColor_SKExtensions.m
+//  Skim
+//
+//  Created by Christiaan Hofman on 6/17/07.
+/*
+ This software is Copyright (c) 2007
+ Christiaan Hofman. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+ - Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+ - Neither the name of Christiaan Hofman nor the names of any
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "NSColor_SKExtensions.h"
+
+
+@implementation NSColor (SKExtensions)
+
++ (id)scriptingRgbaColorWithDescriptor:(NSAppleEventDescriptor *)descriptor {
+    float red, green, blue, alpha = 1.0;
+    switch ([descriptor numberOfItems]) {
+        case 0:
+            switch ([descriptor enumCodeValue]) {
+                case SKASColorRed: return [NSColor redColor];
+                case SKASColorGreen: return [NSColor greenColor];
+                case SKASColorBlue: return [NSColor blueColor];
+                case SKASColorYellow: return [NSColor yellowColor];
+                case SKASColorMagenta: return [NSColor magentaColor];
+                case SKASColorCyan: return [NSColor cyanColor];
+                case SKASColorDarkRed: return [NSColor colorWithDeviceRed:0.5 green:0.0 blue:0.0 alpha:1.0];
+                case SKASColorDarkGreen: return [NSColor colorWithDeviceRed:0.0 green:0.5 blue:0.0 alpha:1.0];
+                case SKASColorDarkBlue: return [NSColor colorWithDeviceRed:0.0 green:0.0 blue:0.5 alpha:1.0];
+                case SKASColorBanana: return [NSColor colorWithDeviceRed:1.0 green:1.0 blue:0.5 alpha:1.0];
+                case SKASColorTurquoise: return [NSColor colorWithDeviceRed:1.0 green:0.5 blue:1.0 alpha:1.0];
+                case SKASColorViolet: return [NSColor colorWithDeviceRed:0.5 green:1.0 blue:1.0 alpha:1.0];
+                case SKASColorOrange: return [NSColor orangeColor];
+                case SKASColorDeepPink: return [NSColor colorWithDeviceRed:1.0 green:0.0 blue:0.5 alpha:1.0];
+                case SKASColorSpringGreen: return [NSColor colorWithDeviceRed:0.0 green:1.0 blue:0.5 alpha:1.0];
+                case SKASColorAqua: return [NSColor colorWithDeviceRed:0.0 green:0.5 blue:1.0 alpha:1.0];
+                case SKASColorLime: return [NSColor colorWithDeviceRed:0.5 green:1.0 blue:0.0 alpha:1.0];
+                case SKASColorDarkViolet: return [NSColor colorWithDeviceRed:0.5 green:0.0 blue:1.0 alpha:1.0];
+                case SKASColorPurple: return [NSColor purpleColor];
+                case SKASColorTeal: return [NSColor colorWithDeviceRed:0.0 green:0.5 blue:0.5 alpha:1.0];
+                case SKASColorOlive: return [NSColor colorWithDeviceRed:0.5 green:0.5 blue:0.0 alpha:1.0];
+                case SKASColorBrown: return [NSColor brownColor];
+                case SKASColorBlack: return [NSColor blackColor];
+                case SKASColorWhite: return [NSColor whiteColor];
+                case SKASColorGray: return [NSColor grayColor];
+                case SKASColorDarkGray: return [NSColor darkGrayColor];
+                case SKASColorLightGray: return [NSColor lightGrayColor];
+                case SKASColorClear: return [NSColor clearColor];
+                default: return nil;
+            }
+            break;
+        case 4:
+            alpha = (float)[[descriptor descriptorAtIndex:4] int32Value] / 65535.0f;
+        case 3:
+            red = (float)[[descriptor descriptorAtIndex:1] int32Value] / 65535.0f;
+            green = (float)[[descriptor descriptorAtIndex:2] int32Value] / 65535.0f;
+            blue = (float)[[descriptor descriptorAtIndex:3] int32Value] / 65535.0f;
+            break;
+        case 2:
+            alpha = (float)[[descriptor descriptorAtIndex:2] int32Value] / 65535.0f;
+        case 1:
+            red = green = blue = (float)[[descriptor descriptorAtIndex:1] int32Value] / 65535.0f;
+            break;
+        default:
+            return nil;
+    }
+    return [NSColor colorWithCalibratedRed:red green:green blue:blue alpha:alpha];
+}
+
+- (id)scriptingRgbaColorDescriptor;
+{
+    float red, green, blue, alpha;
+    [[self colorUsingColorSpaceName:NSCalibratedRGBColorSpace] getRed:&red green:&green blue:&blue alpha:&alpha];
+    
+    NSAppleEventDescriptor *descriptor = [NSAppleEventDescriptor listDescriptor];
+    [descriptor insertDescriptor:[NSAppleEventDescriptor descriptorWithInt32:round(65535 * red)] atIndex:1];
+    [descriptor insertDescriptor:[NSAppleEventDescriptor descriptorWithInt32:round(65535 * green)] atIndex:2];
+    [descriptor insertDescriptor:[NSAppleEventDescriptor descriptorWithInt32:round(65535 * blue)] atIndex:3];
+    [descriptor insertDescriptor:[NSAppleEventDescriptor descriptorWithInt32:round(65535 * alpha)] atIndex:4];
+    
+    return descriptor;
+}
+
+@end
