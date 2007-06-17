6b8db13fe64ea61c5fbc925a4d85a12c80539618
Christiaan Hofman <hofman@lists.sourceforge.net>
2007-06-17 16:42:18 +0000

    Move NSColor category to separate files.

---
+++ NSColor_SKExtensions.h
@@ -0,0 +1,77 @@
+//
+//  NSColor_SKExtensions.h
+//  Skim
+//
+//  Created by Christiaan Hofman on 6/17/07.
+/*
+ This software is Copyright (c) 2007
+ Christiaan Hofman. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+ - Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in
+    the documentation and/or other materials provided with the
+    distribution.
+
+ - Neither the name of Christiaan Hofman nor the names of any
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import <Cocoa/Cocoa.h>
+
+enum {
+    SKASColorRed = 'Red ',
+    SKASColorGreen = 'Gren',
+    SKASColorBlue = 'Blue',
+    SKASColorYellow = 'Yelw',
+    SKASColorMagenta = 'Mgnt',
+    SKASColorCyan = 'Cyan',
+    SKASColorDarkRed = 'DRed',
+    SKASColorDarkGreen = 'DGrn',
+    SKASColorDarkBlue = 'DBlu',
+    SKASColorBanana = 'Bana',
+    SKASColorTurquoise = 'Turq',
+    SKASColorViolet = 'Viol',
+    SKASColorOrange = 'Orng',
+    SKASColorDeepPink = 'DpPk',
+    SKASColorSpringGreen = 'SprG',
+    SKASColorAqua = 'Aqua',
+    SKASColorLime = 'Lime',
+    SKASColorDarkViolet = 'DVio',
+    SKASColorPurple = 'Prpl',
+    SKASColorTeal = 'Teal',
+    SKASColorOlive = 'Oliv',
+    SKASColorBrown = 'Brwn',
+    SKASColorBlack = 'Blck',
+    SKASColorWhite = 'Whit',
+    SKASColorGray = 'Gray',
+    SKASColorLightGray = 'LGry',
+    SKASColorDarkGray = 'DGry',
+    SKASColorClear = 'Clea'
+};
+
+@interface NSColor (SKExtensions)
+
++ (id)scriptingRgbaColorWithDescriptor:(NSAppleEventDescriptor *)descriptor;
+- (id)scriptingRgbaColorDescriptor;
+
+@end
