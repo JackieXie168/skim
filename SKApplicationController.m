5ff89fd85924dded103fe4fea4338392d9c4078a
Christiaan Hofman <hofman@lists.sourceforge.net>
2007-06-18 17:09:52 +0000

    Remove log.

--- SKApplicationController.m
+++ SKApplicationController.m
@@ -52,10 +52,66 @@
 #import "NSBezierPath_BDSKExtensions.h"
 
 
+
+@interface SKGetCommand : NSGetCommand
+@end
+
+
+@implementation SKGetCommand
+
+- (id) performDefaultImplementation {
+        id ret = [super performDefaultImplementation];
+
+        NSAppleEventDescriptor *rtypDesc = [[self appleEvent] paramDescriptorForKeyword:'rtyp'];NSLog(@"directParameter = %@, evaluatedReceivers = %@",[self directParameter],[self evaluatedReceivers]);
+        if( rtypDesc ) { // the get command is requesting a coercion (requested type) 
+                NSScriptClassDescription *classDesc = [[NSScriptSuiteRegistry sharedScriptSuiteRegistry] classDescriptionWithAppleEventCode:[rtypDesc typeCodeValue]];
+                Class class = NULL;
+
+                if( classDesc ) { // found the requested type in the script suites.
+                        class = NSClassFromString( [classDesc className] );
+                } else { // catch some common types that don't have entries in the script suites.
+                        switch( [rtypDesc typeCodeValue] ) {
+                                case 'TEXT': class = [NSString class]; break;
+                                case 'STXT': class = [NSTextStorage class]; break;
+                                case 'nmbr': class = [NSNumber class]; break;
+                                case 'reco': class = [NSDictionary class]; break;
+                                case 'list': class = [NSArray class]; break;
+                                case 'data': class = [NSData class]; break;
+                        }
+                }
+                
+                if ([ret isKindOfClass:[NSAppleEventDescriptor class]])
+                    ret = [self evaluatedReceivers];
+
+                if( class && class != [ret class] ) {
+                        id newRet = [[NSScriptCoercionHandler sharedCoercionHandler] coerceValue:ret toClass:class];
+                        if( newRet ) return newRet;
+                }
+
+                // account for basic types that wont have a coercion handler but have common methods we can use.
+                if( class == [NSString class] && [ret respondsToSelector:@selector( stringValue )] )
+                        return [ret stringValue];
+                else if( class == [NSString class] )
+                        return [ret description];
+                else if( [rtypDesc typeCodeValue] == 'long' && [ret respondsToSelector:@selector( intValue )] )
+                        return [NSNumber numberWithLong:[ret intValue]];
+                else if( [rtypDesc typeCodeValue] == 'sing' && [ret respondsToSelector:@selector( floatValue )] )
+                        return [NSNumber numberWithFloat:[ret floatValue]];
+                else if( ( [rtypDesc typeCodeValue] == 'doub' || [rtypDesc typeCodeValue] == 'nmbr' ) && [ret respondsToSelector:@selector( doubleValue )] )
+                        return [NSNumber numberWithDouble:[ret doubleValue]];
+        }
+
+        return ret;
+}
+
+@end
+
+
 @implementation SKApplicationController
 
 + (void)initialize{
     [self setupDefaults];
+    [SKGetCommand poseAsClass:[NSGetCommand class]];
 }
    
 + (void)setupDefaults{
@@ -156,6 +212,19 @@ static BOOL fileIsInTrash(NSURL *fileURL)
 	if ([[NSUserDefaults standardUserDefaults] boolForKey:SKEnableAppleRemoteKey])
         [[AppleRemote sharedRemote] setDelegate:self];
     [self doSpotlightImportIfNeeded];
+    
+    [[NSScriptCoercionHandler sharedCoercionHandler] registerCoercer:[self class] selector:@selector(coerceNote:toClass:) toConvertFromClass:[PDFAnnotation class] toClass:[NSString class]];
+    [[NSScriptCoercionHandler sharedCoercionHandler] registerCoercer:[self class] selector:@selector(coerceNote:toClass:) toConvertFromClass:[PDFAnnotation class] toClass:[NSTextStorage class]];
+    [[NSScriptCoercionHandler sharedCoercionHandler] registerCoercer:[self class] selector:@selector(coerceNote:toClass:) toConvertFromClass:NSClassFromString(@"SKPDFAnnotationNote") toClass:[NSTextStorage class]];
+}
+
++ (id)coerceNote:(PDFAnnotation *)note toClass:(Class)aClass {NSLog(@"%@",note);
+    if (aClass == [NSString class])
+        return [note contents];
+    else if (aClass == [NSTextStorage class])
+        return [note textContents];
+    else
+        return nil;
 }
 
 - (void)applicationDidBecomeActive:(NSNotification *)aNotification {
