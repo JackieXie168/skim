00694ed36bf9a6a88ba0e6070e71bc93efd38a76
Christiaan Hofman <hofman@lists.sourceforge.net>
2007-09-10 18:01:06 +0000

    Hack to use the background color of a free text annotation for editing.

--- NSCell_SKExtensions.m
+++ NSCell_SKExtensions.m
@@ -40,6 +40,7 @@
 
 #import "NSCell_SKExtensions.h"
 #import "OBUtilities.h"
+#import "SKPDFView.h"
 
 
 @implementation NSCell (SKExtensions)
@@ -55,6 +56,55 @@ static IMP originalHighlightColorWithFrameInView = NULL;
         return [(id)controlView highlightColor];
     else
         return originalHighlightColorWithFrameInView(self, _cmd, cellFrame, controlView);
+}
+
+@end
+
+
+@implementation NSTextFieldCell (SKExtensions)
+
+static IMP originalSetUpFieldEditorAttributes = NULL;
+
++ (void)load {
+    originalSetUpFieldEditorAttributes = OBReplaceMethodImplementationWithSelector(self, @selector(setUpFieldEditorAttributes:), @selector(replacementSetUpFieldEditorAttributes:));
+}
+
+- (NSText *)replacementSetUpFieldEditorAttributes:(NSText *)textObj {
+    textObj = originalSetUpFieldEditorAttributes(self, _cmd, textObj);
+    if ([textObj respondsToSelector:@selector(setBackgroundColor:)] && [[self controlView] respondsToSelector:@selector(setBackgroundColor:)] && [[self controlView] respondsToSelector:@selector(delegate)]) {
+        id delegate = [(NSTextField *)[self controlView] delegate];
+        if ([delegate respondsToSelector:@selector(control:backgroundColorForFieldEditor:)]) {
+            NSColor *color = [delegate control:(NSTextField *)[self controlView] backgroundColorForFieldEditor:textObj];
+            if (color) {
+                [(NSTextField *)[self controlView] setBackgroundColor:color];
+                [(NSTextView *)textObj setBackgroundColor:color];
+            }
+        }
+    }
+    return textObj;
+}
+
+@end
+
+
+@implementation NSTextField (SKExtensions)
+
+static IMP originalSetDelegate = NULL;
+
++ (void)load {
+    originalSetDelegate = OBReplaceMethodImplementationWithSelector(self, @selector(setDelegate:), @selector(replacementSetDelegate:));
+}
+
+- (void)replacementSetDelegate:(id)delegate {
+    originalSetDelegate(self, _cmd, delegate);
+    NSText *currentEditor = [self currentEditor];
+    if (currentEditor && [currentEditor respondsToSelector:@selector(setBackgroundColor:)] && [delegate respondsToSelector:@selector(control:backgroundColorForFieldEditor:)]) {
+        NSColor *color = [delegate control:self backgroundColorForFieldEditor:currentEditor];
+        if (color) {
+            [(NSTextField *)self setBackgroundColor:color];
+            [(NSTextView *)currentEditor setBackgroundColor:color];
+        }
+    }
 }
 
 @end
