af732d1808ac9c5378073b86ba831ef8e93fd7dd
Christiaan Hofman <hofman@lists.sourceforge.net>
2007-12-03 10:22:35 +0000

    Re-enable Actual Size menu item in presentation mode. Always change state of navigation button when the scale changes.

--- SKMainWindowController.m
+++ SKMainWindowController.m
@@ -5253,7 +5253,7 @@ static void removeTemporaryAnnotations(const void *annotation, void *context)
     } else if (action == @selector(doZoomOut:)) {
         return [self isPresentation] == NO && [pdfView canZoomOut];
     } else if (action == @selector(doZoomToActualSize:)) {
-        return [self isPresentation] == NO && fabsf([pdfView scaleFactor] - 1.0 ) > 0.01;
+        return fabsf([pdfView scaleFactor] - 1.0 ) > 0.01;
     } else if (action == @selector(doZoomToPhysicalSize:)) {
         return [self isPresentation] == NO;
     } else if (action == @selector(doZoomToSelection:)) {
