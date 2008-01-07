31e8187a0bb60b4099983a8a63027448d3c9350a
Christiaan Hofman <hofman@lists.sourceforge.net>
2008-01-07 21:47:59 +0000

    Update copyright.

--- QuickLook-Skim/SKQLConverter.h
+++ QuickLook-Skim/SKQLConverter.h
@@ -1,5 +1,5 @@
 /*
- This software is Copyright (c) 2007
+ This software is Copyright (c) 2007-2008
  Christiaan Hofman. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
