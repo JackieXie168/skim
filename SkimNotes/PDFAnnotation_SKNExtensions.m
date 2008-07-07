f78efd39a679d41e03e670796d770bd4cfe4f0dd
hofman <hofman@gmail.com>
2008-07-07 00:10:31 +0000

    Revert last commit, this is probably wrong in GC anyway.

--- SkimNotes/PDFAnnotation_SKNExtensions.m
+++ SkimNotes/PDFAnnotation_SKNExtensions.m
@@ -82,44 +82,31 @@ NSString *SKNPDFAnnotationQuadrilateralPointsKey = @"quadrilateralPoints";
 
 NSString *SKNPDFAnnotationIconTypeKey = @"iconType";
 
-static IMP SKNReplaceMethodImplementationWithSelector(Class aClass, SEL aSelector, SEL impSelector) {
-    Method aMethod = class_getInstanceMethod(aClass, aSelector);
-    Method impMethod = class_getInstanceMethod(aClass, impSelector);
-    IMP anImp = NULL;
-    if (method_getImplementation != NULL && method_setImplementation != NULL) {
-        anImp = method_setImplementation(aMethod, method_getImplementation(impMethod));
-    } else {
-        anImp = aMethod->method_imp;
-        aMethod->method_imp = impMethod->method_imp;
-        // Flush the method cache
-        extern void _objc_flush_caches(Class);
-        if (_objc_flush_caches != NULL)
-            _objc_flush_caches(aClass);
-    }
-    return anImp;
-}
-
 @implementation PDFAnnotation (SKNExtensions)
 
 static CFMutableSetRef SkimNotes = NULL;
 
 static IMP originalDealloc = NULL;
-static IMP originalFinalize = NULL;
 
 - (void)skn_replacementDealloc {
     CFSetRemoveValue(SkimNotes, self);
     originalDealloc(self, _cmd);
 }
 
-- (void)skn_replacementFinalize {
-    CFSetRemoveValue(SkimNotes, self);
-    originalFinalize(self, _cmd);
-}
-
 + (void)load {
-    originalDealloc = SKNReplaceMethodImplementationWithSelector(self, @selector(dealloc), @selector(skn_replacementDealloc));
-    if ([self instancesRespondToSelector:@selector(finalize)])
-        originalFinalize = SKNReplaceMethodImplementationWithSelector(self, @selector(finalize), @selector(skn_replacementFinalize));
+    Method aMethod = class_getInstanceMethod(self, @selector(dealloc));
+    Method impMethod = class_getInstanceMethod(self, @selector(skn_replacementDealloc));
+    IMP anImp = NULL;
+    if (method_getImplementation != NULL && method_setImplementation != NULL) {
+        originalDealloc = method_setImplementation(aMethod, method_getImplementation(impMethod));
+    } else {
+        originalDealloc = aMethod->method_imp;
+        aMethod->method_imp = impMethod->method_imp;
+        // Flush the method cache
+        extern void _objc_flush_caches(Class);
+        if (_objc_flush_caches != NULL)
+            _objc_flush_caches(self);
+    }
     SkimNotes = CFSetCreateMutable(kCFAllocatorDefault, 0, NULL);
 }
 
