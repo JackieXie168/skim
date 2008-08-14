9ad2770c7e1702b1b3832377d04a3beb871c4ee0
hofman <hofman@gmail.com>
2008-08-14 16:35:53 +0000

    fix typo in last commit

--- SKMainWindowController_UI.m
+++ SKMainWindowController_UI.m
@@ -1078,7 +1078,7 @@ static NSString *noteToolImageNames[] = {@"ToolbarTextNoteMenu", @"ToolbarAnchor
         return [self isPresentation] == NO && ([pdfView toolMode] == SKTextToolMode || [pdfView toolMode] == SKNoteToolMode) && [[[pdfView currentSelection] pages] count] && [pdfView hideNotes] == NO;
     } else if (action == @selector(editNote:)) {
         PDFAnnotation *annotation = [pdfView activeAnnotation];
-        return [self isPresentation] == NO && [annotation isSkimNote] && ([[annotation type] isEditable]);
+        return [self isPresentation] == NO && [annotation isSkimNote] && ([annotation isEditable]);
     } else if (action == @selector(toggleHideNotes:)) {
         if ([pdfView hideNotes])
             [menuItem setTitle:NSLocalizedString(@"Show Notes", @"Menu item title")];
