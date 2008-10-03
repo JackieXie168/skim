3fb07ad614bac6f5054f1f624326d60c61979a68
hofman <hofman@gmail.com>
2008-10-03 13:22:03 +0000

    Hide extra cell in note type sheet in nib

--- SKNoteOutlineView.m
+++ SKNoteOutlineView.m
@@ -53,12 +53,7 @@
 }
 
 - (void)awakeFromNib {
-    if (noteTypeMatrix) {
-        id cell = [[[NSActionCell alloc] init] autorelease];
-        [cell setTag:-1];
-        [cell setEnabled:NO];
-        [noteTypeMatrix putCell:cell atRow:4 column:0];
-    } else {
+    if (noteTypeMatrix == nil) {
         [self noteTypeMenu]; // this sets the menu for the header view
         NSNumber *fontSize = [[NSUserDefaults standardUserDefaults] objectForKey:SKTableFontSizeKey];
         if (fontSize)
