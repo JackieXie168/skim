174bd852049a8c846e12306efa563fda0e66d047
hofman <hofman@gmail.com>
2009-01-09 00:23:18 +0000

    update copyright info

--- SKPDFOutline.h
+++ SKPDFOutline.h
@@ -4,7 +4,7 @@
 //
 //  Created by Christiaan Hofman on 9/11/08.
 /*
- This software is Copyright (c) 2008
+ This software is Copyright (c) 2008-2009
  Christiaan Hofman. All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
