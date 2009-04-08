78ff3afa8302e0e74d56686c88620bb94cf7a98b
Christiaan Hofman <hofman@lists.sourceforge.net>
2009-04-08 21:02:07 +0000

    make status bar raised

--- SKStatusBar.m
+++ SKStatusBar.m
@@ -79,10 +79,14 @@
 		[leftCell setFont:[NSFont systemFontOfSize:[NSFont smallSystemFontSize]]];
         [leftCell setAlignment:NSLeftTextAlignment];
         [leftCell setControlView:self];
+        if ([leftCell respondsToSelector:@selector(setBackgroundStyle:)])
+            [leftCell setBackgroundStyle:NSBackgroundStyleRaised];
         rightCell = [[SKStatusTextFieldCell alloc] initTextCell:@""];
 		[rightCell setFont:[NSFont systemFontOfSize:[NSFont smallSystemFontSize]]];
         [rightCell setAlignment:NSRightTextAlignment];
         [rightCell setControlView:self];
+        if ([rightCell respondsToSelector:@selector(setBackgroundStyle:)])
+            [rightCell setBackgroundStyle:NSBackgroundStyleRaised];
 		progressIndicator = nil;
         layer = NULL;
         leftTrackingRectTag = 0;
