b839a7a41800a4d4a0bc470924a8b65c04fbbef0
hofman <hofman@gmail.com>
2009-08-20 20:38:09 +0000

    better solution to Aquqmacs name change. Include both presets in the popup.

--- SKPreferenceController.m
+++ SKPreferenceController.m
@@ -49,9 +49,9 @@
 #define RESETTABLEKEYS_KEY @"ResettableKeys"
 
 static CGFloat SKDefaultFontSizes[] = {8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 16.0, 18.0, 20.0, 24.0, 28.0, 32.0, 48.0, 64.0};
-static NSString *SKTeXEditors[] = {@"TextMate", @"BBEdit", @"TextWrangler", @"Emacs", @"Aquamacs Emacs", @"LyX", @"TeXMaker", @"AlphaX"};
-static NSString *SKTeXEditorCommands[] = {@"mate", @"bbedit", @"edit", @"emacsclient", @"emacsclient", @"lyxeditor", @"texmaker", @"alphac"};
-static NSString *SKTeXEditorArguments[] = {@"-l %line \"%file\"", @"+%line \"%file\"", @"+%line \"%file\"", @"--no-wait +%line \"%file\"", @"--no-wait +%line \"%file\"", @"\"%file\" %line", @"\"%file\" -line %line", @"+%line \"%file\""};
+static NSString *SKTeXEditors[] = {@"TextMate", @"BBEdit", @"TextWrangler", @"Emacs", @"Aquamacs Emacs", @"Aquamacs", @"LyX", @"TeXMaker", @"AlphaX"};
+static NSString *SKTeXEditorCommands[] = {@"mate", @"bbedit", @"edit", @"emacsclient", @"emacsclient", @"emacsclient", @"lyxeditor", @"texmaker", @"alphac"};
+static NSString *SKTeXEditorArguments[] = {@"-l %line \"%file\"", @"+%line \"%file\"", @"+%line \"%file\"", @"+%line \"%file\"", @"--no-wait +%line \"%file\"", @"--no-wait +%line \"%file\"", @"\"%file\" %line", @"\"%file\" -line %line", @"+%line \"%file\""};
 
 #define SKPreferenceWindowFrameAutosaveName @"SKPreferenceWindow"
 
