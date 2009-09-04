0940df998c48342ea35c8ba0ade78255f0d8f78f
hofman <hofman@gmail.com>
2009-09-04 09:38:16 +0000

    just forget about system version check, as we can't give info about it

--- main.m
+++ main.m
@@ -12,16 +12,5 @@
 
 int main(int argc, char *argv[])
 {
-    // Runtime check for system version
-    // - uses Carbon dialog since we don't have NSApp yet
-    // - uses Gestalt so we can check for a specific minor version (if it gets this far, anyway)
-    // - Gestalt header says to read a plist, but mailing lists (and Ali Ozer) say to avoid that
-    SInt32 version;
-    OSStatus err = Gestalt(gestaltSystemVersion, &version);
-    
-    if (noErr != err || version < 0x00001050) {
-        NSLog(@"Incompatible version. Skim requires Mac OSX 10.5 or later.");
-        return err;
-    }
     return NSApplicationMain(argc, (const char **) argv);
 }
