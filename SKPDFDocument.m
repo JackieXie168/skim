40e337f1521feefa017a26f4c33f1e33d583f586
Christiaan Hofman <hofman@lists.sourceforge.net>
2009-09-06 18:43:55 +0000

    add copyright info

--- SKPDFDocument.m
+++ SKPDFDocument.m
@@ -4,7 +4,7 @@
 //
 //  Created by Christiaan on 9/4/09.
 /*
- This software is Copyright (c) 2008-2009
+ This software is Copyright (c) 2009
  Christiaan Hofman. All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
