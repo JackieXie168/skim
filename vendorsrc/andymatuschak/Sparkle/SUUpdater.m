5305eeb7b994f8ba82c84c010209229357aa9442
Christiaan Hofman <hofman@lists.sourceforge.net>
2010-01-02 13:23:17 +0000

    autorelease shared updater

--- vendorsrc/andymatuschak/Sparkle/SUUpdater.m
+++ vendorsrc/andymatuschak/Sparkle/SUUpdater.m
@@ -46,7 +46,7 @@ static NSString *SUUpdaterDefaultsObservationContext = @"SUUpdaterDefaultsObserv
     if (bundle == nil) bundle = [NSBundle mainBundle];
 	id updater = [sharedUpdaters objectForKey:[NSValue valueWithNonretainedObject:bundle]];
 	if (updater == nil)
-		updater = [[[self class] alloc] initForBundle:bundle];
+		updater = [[[[self class] alloc] initForBundle:bundle] autorelease];
 	return updater;
 }
 
