3b8a52e19123615cc906a84b9e68332ce129c092
hofman <hofman@gmail.com>
2010-01-02 12:40:10 +0000

    allow adding tracking rects for whole table row

--- SKTocOutlineView.m
+++ SKTocOutlineView.m
@@ -142,29 +142,44 @@
         [self removeTrackingAreas];
     
     if ([self window]) {
-        NSRange rowRange = [self rowsInRect:[self visibleRect]];
-        NSIndexSet *columnIndexes = [self columnIndexesInRect:[self visibleRect]];
-        NSUInteger row, column = [columnIndexes firstIndex];
+        NSRect visibleRect = [self visibleRect];
+        NSRange rowRange = [self rowsInRect:visibleRect];
+        NSIndexSet *columnIndexes = [self columnIndexesInRect:visibleRect];
+        NSUInteger row, column;
         NSTableColumn *tableColumn;
+        id item;
         id context;
         NSDictionary *userInfo;
         NSRect rect;
+        NSTrackingAreaOptions options = NSTrackingMouseEnteredAndExited | NSTrackingActiveInActiveApp;
         NSTrackingArea *area;
         
-        while (column != NSNotFound) {
-            tableColumn = [[self tableColumns] objectAtIndex:column];
-            for (row = rowRange.location; row < NSMaxRange(rowRange); row++) {
-                if (context = [[self delegate] outlineView:self PDFContextForTableColumn:tableColumn item:[self itemAtRow:row]]) {
-                    userInfo = [[NSDictionary alloc] initWithObjectsAndKeys:context, @"context", nil];
-                    rect = [self frameOfCellAtColumn:column row:row];
-                    area = [[NSTrackingArea alloc] initWithRect:rect options:NSTrackingMouseEnteredAndExited | NSTrackingActiveInActiveApp owner:self userInfo:userInfo];
-                    [self addTrackingArea:area];
-                    [trackingAreas addObject:area];
-                    [area release];
-                    [userInfo release];
+        for (row = rowRange.location; row < NSMaxRange(rowRange); row++) {
+            item = [self itemAtRow:row];
+            if (context = [[self delegate] outlineView:self PDFContextForTableColumn:nil item:item]) {
+                userInfo = [[NSDictionary alloc] initWithObjectsAndKeys:context, @"context", nil];
+                rect = [self rectOfRow:row];
+                area = [[NSTrackingArea alloc] initWithRect:rect options:options owner:self userInfo:userInfo];
+                [self addTrackingArea:area];
+                [trackingAreas addObject:area];
+                [area release];
+                [userInfo release];
+            } else {
+                column = [columnIndexes firstIndex];
+                while (column != NSNotFound) {
+                    tableColumn = [[self tableColumns] objectAtIndex:column];
+                    if (context = [[self delegate] outlineView:self PDFContextForTableColumn:tableColumn item:item]) {
+                        userInfo = [[NSDictionary alloc] initWithObjectsAndKeys:context, @"context", nil];
+                        rect = [self frameOfCellAtColumn:column row:row];
+                        area = [[NSTrackingArea alloc] initWithRect:rect options:options owner:self userInfo:userInfo];
+                        [self addTrackingArea:area];
+                        [trackingAreas addObject:area];
+                        [area release];
+                        [userInfo release];
+                    }
+                    column = [columnIndexes indexGreaterThanIndex:column];
                 }
             }
-            column = [columnIndexes indexGreaterThanIndex:column];
         }
     }
 }
