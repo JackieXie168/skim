d5e735d2c77822e37d93dea0e760595c83127cc8
Christiaan Hofman <hofman@lists.sourceforge.net>
2010-03-13 22:54:52 +0000

    add script phase and a script to generate the strings files for the xib files using ibtool, only run for Release

---
+++ postprocess-xib-strings.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import sys
+import re
+import codecs
+
+argc = len(sys.argv)
+
+if argc != 3:
+    sys.stderr.write('Error: wrong number of arguments.\n')
+    exit(1)
+
+input = sys.argv[1]
+output = sys.argv[2]
+    
+locline = re.compile(r'^"(.*)" = "(.*)";$')
+
+inputfile = codecs.open(input, 'r', 'utf-16')
+outputfile = codecs.open(output, 'w', 'utf-16')
+lines = inputfile.readlines()
+outputfile.seek(0)
+
+for line in lines:
+    line = locline.sub(r'"\2" = "\2";', line, 1)
+    outputfile.write(line)
+
+inputfile.close()
+outputfile.close()
