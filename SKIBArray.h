00f61aa9cce8a3773d6ca59062178483ac4e5c4b
hofman <hofman@gmail.com>
2010-04-16 09:28:04 +0000

    add mutations ivar to array

--- SKIBArray.h
+++ SKIBArray.h
@@ -41,6 +41,7 @@
 
 @interface SKIBArray : NSArray {
     id object[9];
+    unsigned long mutations;
 }
 @property (nonatomic, retain) IBOutlet id object1, object2, object3, object4, object5, object6, object7, object8, object9;
 @end
