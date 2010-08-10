5893b2152bacd2052a2b0d3578e686fb1570d9e0
Christiaan Hofman <hofman@lists.sourceforge.net>
2010-08-10 21:19:47 +0000

    add support for sending email using Mailplane

--- SKMainDocument.m
+++ SKMainDocument.m
@@ -1090,6 +1090,15 @@ static BOOL isIgnorablePOSIXError(NSError *error) {
         [scriptString appendFormat:@"make new enclosure with properties {file:POSIX file \"%@\"}\n", fileName];
         [scriptString appendString:@"end tell\n"];
         [scriptString appendString:@"end tell\n"];
+    } else if ([mailAppName rangeOfString:@"Mailplane" options:NSCaseInsensitiveSearch].length) {
+        scriptString = [NSMutableString stringWithString:@"tell application \"Mailplane\"\n"];
+        [scriptString appendString:@"activate\n"];
+        [scriptString appendFormat:@"set m to make new outgoing message with properties {subject: \"%@\"}\n", subject];
+        [scriptString appendString:@"tell m\n"];
+        //[scriptString appendFormat:@"set content to \"%@\"\n", body];
+        [scriptString appendFormat:@"make new mail attachment with properties {path: \"%@\"}\n", fileName];
+        [scriptString appendString:@"end tell\n"];
+        [scriptString appendString:@"end tell\n"];
     } else {
         scriptString = [NSMutableString stringWithString:@"tell application \"Mail\"\n"];
         [scriptString appendString:@"activate\n"];
