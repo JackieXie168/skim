a7b31080d4810ceddf464c73c2b25f9de602eed5
Christiaan Hofman <hofman@lists.sourceforge.net>
2011-03-12 22:01:11 +0000

    Don't generate empty .strings files for NIBs

--- postprocess-xib-strings.py
+++ postprocess-xib-strings.py
@@ -20,28 +20,30 @@ inputfile = codecs.open(input, 'r', 'utf-16')
 lines = inputfile.readlines()
 inputfile.close()
 
-outputfile = codecs.open(output, 'w', 'utf-16')
-outputfile.seek(0)
-
-foundstrings = set()
-comment = None
-
-for line in lines:
-    if commentre.match(line) != None:
-        comment = line
-    else:
-        match = locre.match(line)
-        if match != None:
-            string = match.group(2)
-            if string not in foundstrings:
-                foundstrings.add(string)
-                outputfile.write('\n')
-                if comment != None:
-                    outputfile.write(comment)
-                line = match.expand(r'"\2" = "\2";\n')
-                outputfile.write(line)
-            comment = None
-        elif line != '\n':
-            sys.stderr.write('warning: confused about line in {0}:\n{1}\n'.format(input, line))
-
-outputfile.close()
+if len (lines) > 0:
+    
+    outputfile = codecs.open(output, 'w', 'utf-16')
+    outputfile.seek(0)
+    
+    foundstrings = set()
+    comment = None
+    
+    for line in lines:
+        if commentre.match(line) != None:
+            comment = line
+        else:
+            match = locre.match(line)
+            if match != None:
+                string = match.group(2)
+                if string not in foundstrings:
+                    foundstrings.add(string)
+                    outputfile.write('\n')
+                    if comment != None:
+                        outputfile.write(comment)
+                    line = match.expand(r'"\2" = "\2";\n')
+                    outputfile.write(line)
+                comment = None
+            elif line != '\n':
+                sys.stderr.write('warning: confused about line in {0}:\n{1}\n'.format(input, line))
+    
+    outputfile.close()
