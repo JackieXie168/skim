cd4c1f3c94462cac08a1de1a52d55114167d75c8
hofman <hofman@gmail.com>
2011-04-28 13:12:33 +0000

    simplify find field editor class even more, set fieldeditor flag when creating

--- SKFindFieldEditor.m
+++ SKFindFieldEditor.m
@@ -41,15 +41,6 @@
 
 @implementation SKFindFieldEditor
 
-- (id)initWithFrame:(NSRect)frameRect {
-	self = [super initWithFrame:frameRect];
-    if (self) {
-		[self setFieldEditor:YES];
-		[self setUsesFindPanel:NO];
-	}
-	return self;
-}
-
 - (BOOL)respondsToSelector:(SEL)aSelector {
     return aSelector != @selector(performFindPanelAction:) && [super respondsToSelector:aSelector];
 }
