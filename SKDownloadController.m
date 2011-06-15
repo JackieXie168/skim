b6992e3d0a03cc8cf02f29b8e7ad186a2c073f15
Christiaan Hofman <hofman@lists.sourceforge.net>
2011-06-15 13:16:53 +0000

    Fix check for dependent keys

--- SKDownloadController.m
+++ SKDownloadController.m
@@ -348,8 +348,6 @@ static SKDownloadController *sharedDownloadController = nil;
                 [[self window] close];
         }
     }
-    // binding does not notice the change to canRemove
-    [clearButton setEnabled:[[downloads valueForKeyPath:@"@max.canRemove"] boolValue]];
 }
 
 #pragma mark NSTableViewDataSource
