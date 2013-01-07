85fbb7f2b53e99b801ef0edd195acf51e9911ca4
Christiaan Hofman <hofman@lists.sourceforge.net>
2013-01-07 01:21:41 +0000

    update copyright dates

--- SKDragImageView.m
+++ SKDragImageView.m
@@ -4,7 +4,7 @@
 //
 //  Created by Christiaan Hofman on 11/28/05.
 /*
- This software is Copyright (c) 2005-2012
+ This software is Copyright (c) 2005-2013
  Christiaan Hofman. All rights reserved.
 
  Redistribution and use in source and binary forms, with or without
