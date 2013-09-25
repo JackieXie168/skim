4df078de0f323bab570db6e02fde3c9fc16f27e7
hofman <hofman@gmail.com>
2013-09-25 11:37:49 +0000

    convenience method to see whether we should use a transition

--- SKTransitionController.h
+++ SKTransitionController.h
@@ -86,6 +86,7 @@ typedef NSUInteger SKAnimationTransitionStyle;
 @property (nonatomic) CGFloat duration;
 @property (nonatomic) BOOL shouldRestrict;
 @property (nonatomic, copy) NSArray *pageTransitions;
+@property (nonatomic) BOOL hasTransition;
 
 + (NSArray *)transitionFilterNames;
 + (NSArray *)transitionNames;
