f437edefcbf0bb9a922e7215d31d7120e270a37f
Christiaan Hofman <hofman@lists.sourceforge.net>
2013-10-20 08:39:58 +0000

    only use PDF initial view settings when hidden pref is set rather than the other way around

--- SKMainWindowController.m
+++ SKMainWindowController.m
@@ -170,7 +170,7 @@ static char SKMainWindowDefaultsObservationContext;
 
 #define SKDisableTableToolTipsKey @"SKDisableTableToolTips"
 
-#define SKIgnoreSettingsFromPDFKey @"SKIgnoreSettingsFromPDF"
+#define SKUseSettingsFromPDFKey @"SKUseSettingsFromPDF"
 
 static void addSideSubview(NSView *view, NSView *contentView, BOOL usesDrawers) {
     NSRect rect = [contentView bounds];
@@ -450,7 +450,7 @@ static void addSideSubview(NSView *view, NSView *contentView, BOOL usesDrawers) 
     [pdfContentView addSubview:pdfView];
     
     // get the initial display mode from the PDF if present and not overridden by an explicit setup
-    if (hasWindowSetup == NO && [[NSUserDefaults standardUserDefaults] boolForKey:SKIgnoreSettingsFromPDFKey] == NO) {
+    if (hasWindowSetup == NO && [[NSUserDefaults standardUserDefaults] boolForKey:SKUseSettingsFromPDFKey]) {
         NSDictionary *initialSettings = [[self pdfDocument] initialSettings];
         if (initialSettings)
             [self applyPDFSettings:initialSettings];
