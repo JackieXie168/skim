865cf57c133f058352050bb1d70e1640b34af53d
hofman <hofman@gmail.com>
2015-01-02 22:21:54 +0000

    Update copyright dates

--- SKNAgentListener.m
+++ SKNAgentListener.m
@@ -3,7 +3,7 @@
  *
  *  Created by Adam Maxwell on 04/10/07.
  *
- This software is Copyright (c) 2007-2014
+ This software is Copyright (c) 2007-2015
  Adam Maxwell. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
