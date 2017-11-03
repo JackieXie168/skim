3c1ae2e4cff5fefd1f1d4800b1dc5556b689c450
hofman <hofman@gmail.com>
2017-11-03 16:17:18 +0000

    fix workaround for PDFBorder crasher, on 10.13 it now uses NSInteger for the count

--- SkimNotes/PDFAnnotation_SKNExtensions.m
+++ SkimNotes/PDFAnnotation_SKNExtensions.m
@@ -842,7 +842,7 @@ static NSArray *replacement_dashPattern(id self, SEL _cmd) {
     Class cls = NSClassFromString(@"PDFBorderPrivateVars");
     if (cls) {
         Ivar dashCountIvar = class_getInstanceVariable(cls, "dashCount");
-        if (dashCountIvar && 0 != strcmp(ivar_getTypeEncoding(dashCountIvar), @encode(NSUInteger)))
+        if (dashCountIvar && 0 == strcmp(ivar_getTypeEncoding(dashCountIvar), @encode(unsigned int)))
             class_replaceMethod(self, @selector(dashPattern), (IMP)replacement_dashPattern, "@@:");
     }
 }
