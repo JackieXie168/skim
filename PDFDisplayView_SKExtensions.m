cd5ecc3d4dd7a37ee8298f6a0652e5e809d3c11a
hofman <hofman@gmail.com>
2017-11-27 18:30:53 +0000

    default to 10.13 ivar layout

--- PDFDisplayView_SKExtensions.m
+++ PDFDisplayView_SKExtensions.m
@@ -67,7 +67,7 @@ static id SKGetPDFView(id self) {
     } else if ([self respondsToSelector:@selector(pdfView)]) {
         pdfView = [self pdfView];
     } else {
-        @try { pdfView = [self valueForKey:@"pdfView"] ?: [self valueForKeyPath:@"private.pdfView"]; }
+        @try { pdfView = [self valueForKeyPath:@"private.pdfView"] ?: [self valueForKey:@"pdfView"]; }
         @catch (id exception) {}
     }
     return pdfView;
