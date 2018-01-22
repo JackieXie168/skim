c8e728865c47fd69b4db14028a9764c0872f77dd
hofman <hofman@gmail.com>
2018-01-22 15:14:41 +0000

    delegate is already nil as it was never set

--- SKSnapshotWindowController.m
+++ SKSnapshotWindowController.m
@@ -96,12 +96,9 @@ static char SKSnaphotWindowDefaultsObservationContext;
 
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver: self];
-    SKENSURE_MAIN_THREAD(
-        [pdfView setDelegate:nil];
-        // Yosemite and El Capitan have a retain cycle when we leave the PDFView with a document
-        if (RUNNING_AFTER(10_9) && RUNNING_BEFORE(10_12))
-            [pdfView setDocument:nil];
-    );
+    // Yosemite and El Capitan have a retain cycle when we leave the PDFView with a document
+    if (RUNNING_AFTER(10_9) && RUNNING_BEFORE(10_12))
+        SKENSURE_MAIN_THREAD( [pdfView setDocument:nil]; );
     delegate = nil;
     SKDESTROY(thumbnail);
     SKDESTROY(pageLabel);
