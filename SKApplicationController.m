3e058fcf6eb5bded326406991d5992abb2824cfd
hofman <hofman@gmail.com>
2018-10-21 17:14:41 +0000

    remove import

--- SKApplicationController.m
+++ SKApplicationController.m
@@ -73,7 +73,6 @@
 #import "NSError_SKExtensions.h"
 #import "NSValueTransformer_SKExtensions.h"
 #import "SKAnimatedBorderlessWindow.h"
-#import "PDFView_SKExtensions.h"
 
 #define WEBSITE_URL @"https://skim-app.sourceforge.io/"
 #define WIKI_URL    @"https://sourceforge.net/p/skim-app/wiki/"
