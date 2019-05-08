16e1c4188572ec9d7b192283b78c2fdf324d1e32
hofman <hofman@gmail.com>
2019-05-08 21:43:48 +0000

    revert part of last commit that should not have happened

--- SKDownloadController.m
+++ SKDownloadController.m
@@ -147,8 +147,6 @@ FOUNDATION_EXPORT NSString * const NSURLSessionDownloadTaskResumeData NS_AVAILAB
 #define SKDownloadsToolbarClearItemIdentifier       @"SKDownloadsToolbarClearItemIdentifier"
 
 #define SKDownloadsTouchBarClearItemIdentifier      @"SKDownloadsTouchBarClearItemIdentifier"
-#define SKDownloadsTouchBarResumeItemIdentifier     @"SKDownloadsTouchBarResumeItemIdentifier"
-#define SKDownloadsTouchBarCancelItemIdentifier     @"SKDownloadsTouchBarCancelItemIdentifier"
 
 #define PROGRESS_COLUMN 1
 #define RESUME_COLUMN   2
