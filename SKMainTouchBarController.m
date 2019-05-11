2feddb3f6bd957721b7874f8054e9760bbe67d8f
hofman <hofman@gmail.com>
2019-05-11 21:48:47 +0000

    revert previous commit, as this does not worl well with momentary segmented controls

--- SKMainTouchBarController.m
+++ SKMainTouchBarController.m
@@ -246,7 +246,6 @@ enum {
             item = [[[NSClassFromString(@"NSPopoverTouchBarItem") alloc] initWithIdentifier:identifier] autorelease];
             [(NSPopoverTouchBarItem *)item setCollapsedRepresentationImage:[NSImage imageNamed:@"NSTouchBarAddTemplate"]];
             [(NSPopoverTouchBarItem *)item setPopoverTouchBar:popoverTouchBar];
-            [(NSPopoverTouchBarItem *)item setPressAndHoldTouchBar:popoverTouchBar];
             [(NSPopoverTouchBarItem *)item setCustomizationLabel:NSLocalizedString(@"Add Note", @"Toolbar item label")];
             
         } else if ([identifier isEqualToString:SKDocumentTouchBarAddNoteItemIdentifier]) {
