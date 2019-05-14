6b24c94d21b2d9020ff308a1d70d7d309650dd3f
hofman <hofman@gmail.com>
2019-05-14 17:03:04 +0000

    rename variable

--- SKMainTouchBarController.m
+++ SKMainTouchBarController.m
@@ -349,14 +349,14 @@ enum {
         [colorCell setShouldFill:YES];
         [imageView setCell:colorCell];
         [colorCell release];
-        NSMutableArray *contraints = [NSMutableArray array];
+        NSMutableArray *constraints = [NSMutableArray array];
         [imageView setTranslatesAutoresizingMaskIntoConstraints:NO];
         [itemView addSubview:imageView];
-        [contraints addObject:[NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:itemView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0.0]];
-        [contraints addObject:[NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:itemView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0.0]];
-        [contraints addObject:[NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:itemView attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0]];
-        [contraints addObject:[NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:itemView attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0]];
-        [NSLayoutConstraint activateConstraints:contraints];
+        [constraints addObject:[NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeLeading relatedBy:NSLayoutRelationEqual toItem:itemView attribute:NSLayoutAttributeLeading multiplier:1.0 constant:0.0]];
+        [constraints addObject:[NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeTrailing relatedBy:NSLayoutRelationEqual toItem:itemView attribute:NSLayoutAttributeTrailing multiplier:1.0 constant:0.0]];
+        [constraints addObject:[NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:itemView attribute:NSLayoutAttributeTop multiplier:1.0 constant:0.0]];
+        [constraints addObject:[NSLayoutConstraint constraintWithItem:imageView attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:itemView attribute:NSLayoutAttributeBottom multiplier:1.0 constant:0.0]];
+        [NSLayoutConstraint activateConstraints:constraints];
     }
     [imageView setObjectValue:[[self colors] objectAtIndex:idx]];
     return itemView;
