5ae2839353599d30d3a7c57af33cf64755ab8217
hofman <hofman@gmail.com>
2019-05-23 14:17:45 +0000

    change cube transition, to collapse sides at t=0 and 1

--- SkimTransitions/CubeTransition/SKTCubeTransitionFilter.m
+++ SkimTransitions/CubeTransition/SKTCubeTransitionFilter.m
@@ -39,6 +39,25 @@
             nil];
 }
 
+// we actually draw a pentagon, because we want to collapse the side at t=0 and t=1
+static CGFloat ANGLE = 0.0;
+static CGFloat SIN_1 = 0.0;
+static CGFloat COS_1 = 0.0;
+static CGFloat SIN_3 = 0.0;
+static CGFloat COS_3 = 0.0;
+static CGFloat A = 0.0;
+static CGFloat B = 0.0;
+
++ (void)initialize {
+    ANGLE = 0.2 * M_PI;
+    SIN_1 = sin(ANGLE);
+    COS_1 = cos(ANGLE);
+    SIN_3 = sin(3.0 * ANGLE);
+    COS_3 = cos(3.0 * ANGLE);
+    A = 1.0 / (SIN_1 * SIN_1);
+    B = (2.0 - A) / COS_1;
+}
+
 // called when setting up for fragment program and also calls fragment program
 - (CIImage *)outputImage
 {
@@ -48,19 +67,16 @@
     CGFloat height = [inputExtent W];
     CGFloat x = [inputExtent X] + 0.5 * width;
     CGFloat y = [inputExtent Y] + 0.5 * height;
-    CGFloat amount = 1.0;
-    CGFloat angle1 = (0.5 - t) * M_PI_2;
-    CGFloat angle2 = (0.5 + t) * M_PI_2;
-    CGFloat s1 = sin(angle1);
-    CGFloat c1 = cos(angle1);
-    CGFloat s2 = sin(angle2);
-    CGFloat c2 = cos(angle2);
-    CGFloat x1 = -M_SQRT2 * width * s2 / (2.0 - amount * (M_SQRT2 * c2 - 1.0));
-    CGFloat x2 = M_SQRT2 * width * s1 / (2.0 - amount * (M_SQRT2 * c1 - 1.0));
-    CGFloat x3 = M_SQRT2 * width * s2 / (2.0 + amount * (M_SQRT2 * c2 + 1.0));
-    CGFloat y1 = height / (2.0 - amount * (M_SQRT2 * c2 - 1.0));
-    CGFloat y2 = height / (2.0 - amount * (M_SQRT2 * c1 - 1.0));
-    CGFloat y3 = height / (2.0 + amount * (M_SQRT2 * c2 + 1.0));
+    CGFloat angle = 2.0 * ANGLE * t;
+    CGFloat s = sin(angle);
+    CGFloat c = cos(angle);
+    CGFloat C = width / (SIN_1 * height);
+    CGFloat y1 = height / (A + B * (COS_1 * c - SIN_1 * s));
+    CGFloat y2 = height / (A + B * (COS_1 * c + SIN_1 * s));
+    CGFloat y3 = height / (A + B * (COS_3 * c + SIN_3 * s));
+    CGFloat x1 = C * y1 * (- SIN_1 * c - COS_1 * s);
+    CGFloat x2 = C * y2 * (SIN_1 * c - COS_1 * s);
+    CGFloat x3 = C * y3 * (SIN_3 * c - COS_3 * s);
     
     CIFilter *perspectiveFilter1 = [CIFilter filterWithName:@"CIPerspectiveTransformWithExtent"];
     [perspectiveFilter1 setValue:inputImage forKey:@"inputImage"];
@@ -72,24 +88,24 @@
     
     if (moveRight) {
         [perspectiveFilter1 setValue:[CIVector vectorWithX:x - x2 Y:y + y2] forKey:@"inputTopLeft"];
-        [perspectiveFilter1 setValue:[CIVector vectorWithX:x - x1 Y:y + y1] forKey:@"inputTopRight"];
         [perspectiveFilter1 setValue:[CIVector vectorWithX:x - x2 Y:y - y2] forKey:@"inputBottomLeft"];
+        [perspectiveFilter1 setValue:[CIVector vectorWithX:x - x1 Y:y + y1] forKey:@"inputTopRight"];
         [perspectiveFilter1 setValue:[CIVector vectorWithX:x - x1 Y:y - y1] forKey:@"inputBottomRight"];
         [perspectiveFilter2 setValue:[CIVector vectorWithX:x - x3 Y:y + y3] forKey:@"inputTopLeft"];
-        [perspectiveFilter2 setValue:[CIVector vectorWithX:x - x2 Y:y + y2] forKey:@"inputTopRight"];
         [perspectiveFilter2 setValue:[CIVector vectorWithX:x - x3 Y:y - y3] forKey:@"inputBottomLeft"];
+        [perspectiveFilter2 setValue:[CIVector vectorWithX:x - x2 Y:y + y2] forKey:@"inputTopRight"];
         [perspectiveFilter2 setValue:[CIVector vectorWithX:x - x2 Y:y - y2] forKey:@"inputBottomRight"];
     } else {
         [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x1 Y:y + y1] forKey:@"inputTopLeft"];
-        [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x2 Y:y + y2] forKey:@"inputTopRight"];
         [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x1 Y:y - y1] forKey:@"inputBottomLeft"];
+        [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x2 Y:y + y2] forKey:@"inputTopRight"];
         [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x2 Y:y - y2] forKey:@"inputBottomRight"];
         [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x2 Y:y + y2] forKey:@"inputTopLeft"];
-        [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x3 Y:y + y3] forKey:@"inputTopRight"];
         [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x2 Y:y - y2] forKey:@"inputBottomLeft"];
+        [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x3 Y:y + y3] forKey:@"inputTopRight"];
         [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x3 Y:y - y3] forKey:@"inputBottomRight"];
     }
-
+    
     CIFilter *compositingFilter = [CIFilter filterWithName:@"CISourceOverCompositing"];
     [compositingFilter setValue:[t < 0.5 ? perspectiveFilter1 : perspectiveFilter2 valueForKey:@"outputImage"] forKey:@"inputImage"];
     [compositingFilter setValue:[t < 0.5 ? perspectiveFilter2 : perspectiveFilter1 valueForKey:@"outputImage"] forKey:@"inputBackgroundImage"];
