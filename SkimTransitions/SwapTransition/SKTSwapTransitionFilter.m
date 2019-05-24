7b599edecdfccff40e9258a386e4ff69a8800ecb
hofman <hofman@gmail.com>
2019-05-24 15:49:01 +0000

    angle attribute for flip transition, do some fading

--- SkimTransitions/SwapTransition/SKTSwapTransitionFilter.m
+++ SkimTransitions/SwapTransition/SKTSwapTransitionFilter.m
@@ -10,6 +10,11 @@
 #import <Foundation/Foundation.h>
 #import <ApplicationServices/ApplicationServices.h>
 
+#define kCIInputTopLeftKey @"inputTopLeft"
+#define kCIInputTopRightKey @"inputTopRight"
+#define kCIInputBottomLeftKey @"inputBottomLeft"
+#define kCIInputBottomRightKey @"inputBottomRight"
+
 @implementation SKTSwapTransitionFilter
 
 - (NSDictionary *)customAttributes
@@ -19,7 +24,17 @@
             [NSDictionary dictionaryWithObjectsAndKeys:
                  [CIVector vectorWithX:0.0 Y:0.0 Z:300.0 W:300.0], kCIAttributeDefault,
                  kCIAttributeTypeRectangle,          kCIAttributeType,
-                 nil],                               @"inputExtent",
+                 nil],                               kCIInputExtentKey,
+            
+            [NSDictionary dictionaryWithObjectsAndKeys:
+                 [NSNumber numberWithDouble:  0.0], kCIAttributeMin,
+                 [NSNumber numberWithDouble:  0.0], kCIAttributeMax,
+                 [NSNumber numberWithDouble:  -M_PI], kCIAttributeSliderMin,
+                 [NSNumber numberWithDouble:  M_PI], kCIAttributeSliderMax,
+                 [NSNumber numberWithDouble:  0.0], kCIAttributeDefault,
+                 [NSNumber numberWithDouble:  0.0], kCIAttributeIdentity,
+                 kCIAttributeTypeAngle,             kCIAttributeType,
+                 nil],                              kCIInputAngleKey,
             
             [NSDictionary dictionaryWithObjectsAndKeys:
                  [NSNumber numberWithDouble:  0.0], kCIAttributeMin,
@@ -29,66 +44,97 @@
                  [NSNumber numberWithDouble:  0.0], kCIAttributeDefault,
                  [NSNumber numberWithDouble:  0.0], kCIAttributeIdentity,
                  kCIAttributeTypeTime,              kCIAttributeType,
-                 nil],                              @"inputTime",
-            
-            [NSDictionary dictionaryWithObjectsAndKeys:
-                 [NSNumber numberWithBool:NO], kCIAttributeDefault,
-                 kCIAttributeTypeBoolean,          kCIAttributeType,
-                 nil],                               @"inputRight",
+                 nil],                              kCIInputTimeKey,
             
             nil];
+}
+
+static inline NSInteger directionForAngles(CGFloat angle, CGFloat cornerAngle) {
+    while (angle <= M_PI)
+        angle += 2.0 * M_PI;
+    while (angle > M_PI)
+        angle -= 2.0 * M_PI;
+    if (angle > cornerAngle) {
+        if (angle < M_PI - cornerAngle)
+            return 1;
+        else
+            return 2;
+    } else if (angle < -cornerAngle) {
+        if (angle > cornerAngle - M_PI)
+            return 3;
+        else
+            return 2;
+    }
+    return 0;
 }
 
 // called when setting up for fragment program and also calls fragment program
 - (CIImage *)outputImage
 {
     CGFloat t = [inputTime doubleValue];
-    BOOL swapRight = [inputRight boolValue];
+    CGFloat angle = [inputAngle doubleValue];
     CGFloat width = [inputExtent Z];
     CGFloat height = [inputExtent W];
     CGFloat r = width;
     CGFloat x = [inputExtent X] + 0.5 * width;
     CGFloat y = [inputExtent Y] + 0.5 * height;
-    CGFloat amount = 1.0;
-    CGFloat angle = swapRight ? -t * M_PI : t * M_PI;
-    CGFloat s = sin(angle);
-    CGFloat c = cos(angle);
-    CGFloat x1 = (- width - r * s) / (2.0 - amount * (c - 1.0));
-    CGFloat x2 = (width - r * s) / (2.0 - amount * (c - 1.0));
-    CGFloat x3 = (- width + r * s) / (2.0 + amount * (c + 1.0));
-    CGFloat x4 = (width + r * s) / (2.0 + amount * (c + 1.0));
-    CGFloat y1 = height / (2.0 - amount * (c - 1.0));
-    CGFloat y3 = height / (2.0 + amount * (c + 1.0));
-    CIVector *top1 = [CIVector vectorWithX:x + x1 Y:y + y1];
-    CIVector *bottom1 = [CIVector vectorWithX:x + x1 Y:y - y1];
-    CIVector *top2 = [CIVector vectorWithX:x + x2 Y:y + y1];
-    CIVector *bottom2 = [CIVector vectorWithX:x + x2 Y:y - y1];
-    CIVector *top3 = [CIVector vectorWithX:x + x3 Y:y + y3];
-    CIVector *bottom3 = [CIVector vectorWithX:x + x3 Y:y - y3];
-    CIVector *top4 = [CIVector vectorWithX:x + x4 Y:y + y3];
-    CIVector *bottom4 = [CIVector vectorWithX:x + x4 Y:y - y3];
-
+    NSInteger direction = directionForAngles(angle, atan2(height, width));
+    CGFloat a = direction > 1 ? -M_PI * t : M_PI * t;
+    CGFloat s = sin(a);
+    CGFloat c = cos(a);
+    CGFloat f1 = 1.0 / (2.0 - (c - 1.0));
+    CGFloat f2 = 1.0 / (2.0 + (c + 1.0));
+    CGFloat tn = 1.0;
+    CGFloat cotn = 1.0;
+    
+    if (direction % 2 == 0) {
+        tn = tan(angle);
+        r = width;
+    } else {
+        cotn = 1.0 / tan(angle);
+        r = height;
+    }
+    
+    CGFloat x1 = f1 * (- width - r * s * cotn);
+    CGFloat x2 = f1 * (width - r * s * cotn);
+    CGFloat x3 = f2 * (- width + r * s * cotn);
+    CGFloat x4 = f2 * (width + r * s * cotn);
+    CGFloat y1 = f1 * (height - r * s * tn);
+    CGFloat y2 = f1 * (- height - r * s * tn);
+    CGFloat y3 = f2 * (height + r * s * tn);
+    CGFloat y4 = f2 * (- height + r * s * tn);
+    
     CIFilter *perspectiveFilter1 = [CIFilter filterWithName:@"CIPerspectiveTransformWithExtent"];
-    [perspectiveFilter1 setValue:inputImage forKey:@"inputImage"];
-    [perspectiveFilter1 setValue:inputExtent forKey:@"inputExtent"];
-    [perspectiveFilter1 setValue:top1 forKey:@"inputTopLeft"];
-    [perspectiveFilter1 setValue:top2 forKey:@"inputTopRight"];
-    [perspectiveFilter1 setValue:bottom1 forKey:@"inputBottomLeft"];
-    [perspectiveFilter1 setValue:bottom2 forKey:@"inputBottomRight"];
+    [perspectiveFilter1 setValue:inputImage forKey:kCIInputImageKey];
+    [perspectiveFilter1 setValue:inputExtent forKey:kCIInputExtentKey];
+    [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x1 Y:y + y1] forKey:kCIInputTopLeftKey];
+    [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x2 Y:y + y1] forKey:kCIInputTopRightKey];
+    [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x1 Y:y + y2] forKey:kCIInputBottomLeftKey];
+    [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x2 Y:y + y2] forKey:kCIInputBottomRightKey];
     
     CIFilter *perspectiveFilter2 = [CIFilter filterWithName:@"CIPerspectiveTransformWithExtent"];
-    [perspectiveFilter2 setValue:inputTargetImage forKey:@"inputImage"];
-    [perspectiveFilter2 setValue:inputExtent forKey:@"inputExtent"];
-    [perspectiveFilter2 setValue:top3 forKey:@"inputTopLeft"];
-    [perspectiveFilter2 setValue:top4 forKey:@"inputTopRight"];
-    [perspectiveFilter2 setValue:bottom3 forKey:@"inputBottomLeft"];
-    [perspectiveFilter2 setValue:bottom4 forKey:@"inputBottomRight"];
+    [perspectiveFilter2 setValue:inputTargetImage forKey:kCIInputImageKey];
+    [perspectiveFilter2 setValue:inputExtent forKey:kCIInputExtentKey];
+    [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x3 Y:y + y3] forKey:kCIInputTopLeftKey];
+    [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x4 Y:y + y3] forKey:kCIInputTopRightKey];
+    [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x3 Y:y + y4] forKey:kCIInputBottomLeftKey];
+    [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x4 Y:y + y4] forKey:kCIInputBottomRightKey];
     
     CIFilter *compositingFilter = [CIFilter filterWithName:@"CISourceOverCompositing"];
-    [compositingFilter setValue:[t < 0.5 ? perspectiveFilter1 : perspectiveFilter2 valueForKey:@"outputImage"] forKey:@"inputImage"];
-    [compositingFilter setValue:[t < 0.5 ? perspectiveFilter2 : perspectiveFilter1 valueForKey:@"outputImage"] forKey:@"inputBackgroundImage"];
+    CIFilter *dissolveFilter = [CIFilter filterWithName:@"CIDissolveTransition"];
+    [dissolveFilter setValue:[[CIImage alloc] init] forKey:kCIInputTargetImageKey];
+    [dissolveFilter setValue:[NSNumber numberWithDouble:(2.0 * t - 1.0) * (2.0 * t - 1.0)] forKey:kCIInputTimeKey];
+    if (t < 0.5) {
+        [dissolveFilter setValue:[perspectiveFilter2 valueForKey:kCIOutputImageKey] forKey:kCIInputImageKey];
+        [compositingFilter setValue:[perspectiveFilter1 valueForKey:kCIOutputImageKey] forKey:kCIInputImageKey];
+        [compositingFilter setValue:[dissolveFilter valueForKey:kCIOutputImageKey] forKey:kCIInputBackgroundImageKey];
+    } else {
+        [dissolveFilter setValue:[perspectiveFilter1 valueForKey:kCIOutputImageKey] forKey:kCIInputImageKey];
+        [compositingFilter setValue:[perspectiveFilter2 valueForKey:kCIOutputImageKey] forKey:kCIInputImageKey];
+        [compositingFilter setValue:[dissolveFilter valueForKey:kCIOutputImageKey] forKey:kCIInputBackgroundImageKey];
+    }
     
-    return [compositingFilter valueForKey:@"outputImage"];
+    return [compositingFilter valueForKey:kCIOutputImageKey];
 }
 
 @end
