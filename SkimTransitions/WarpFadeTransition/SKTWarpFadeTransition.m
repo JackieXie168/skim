6c63bc979711c85b410345632c62ad6ea54cf527
hofman <hofman@gmail.com>
2019-05-26 21:05:16 +0000

    dissolve warp fade transition in the middle

--- SkimTransitions/WarpFadeTransition/SKTWarpFadeTransition.m
+++ SkimTransitions/WarpFadeTransition/SKTWarpFadeTransition.m
@@ -46,12 +46,13 @@
     CGFloat width = [inputExtent Z];
     CGFloat height = [inputExtent W];
     CGFloat angle = (1.0 - (2.0 * t - 1.0) * (2.0 * t - 1.0)) * 2.0 * M_PI;
+    CGFloat t1 = fmin(fmax(2.0 * (t - 0.25), 0.0), 1.0);
     CGFloat radius = fmax(width, height);
     
     CIFilter *dissolveFilter = [CIFilter filterWithName:@"CIDissolveTransition"];
     [dissolveFilter setValue:inputImage forKey:kCIInputImageKey];
     [dissolveFilter setValue:inputTargetImage forKey:kCIInputTargetImageKey];
-    [dissolveFilter setValue:inputTime forKey:kCIInputTimeKey];
+    [dissolveFilter setValue:[NSNumber numberWithDouble:t1] forKey:kCIInputTimeKey];
 
     CIFilter *twirlFilter = [CIFilter filterWithName:@"CITwirlDistortion"];
     [twirlFilter setValue:[dissolveFilter valueForKey:kCIOutputImageKey] forKey:kCIInputImageKey];
