4a30fbc206ad69982070ac3d5982a3cc3d4b7af5
hofman <hofman@gmail.com>
2019-05-28 13:05:17 +0000

    use CIPerspectiveTransform for Swap transition

--- SkimTransitions/SwapTransition/SKTSwapTransition.m
+++ SkimTransitions/SwapTransition/SKTSwapTransition.m
@@ -75,46 +75,46 @@ static inline NSInteger directionForAngles(CGFloat angle, CGFloat cornerAngle) {
     CGFloat angle = [inputAngle doubleValue];
     CGFloat width = [inputExtent Z];
     CGFloat height = [inputExtent W];
-    CGFloat r = width;
+    CGFloat r = 0.5 * width;
     CGFloat x = [inputExtent X] + 0.5 * width;
     CGFloat y = [inputExtent Y] + 0.5 * height;
     NSInteger direction = directionForAngles(angle, atan2(height, width));
     CGFloat a = direction > 1 ? -M_PI * t : M_PI * t;
     CGFloat s = sin(a);
     CGFloat c = cos(a);
-    CGFloat f1 = 1.0 / (2.0 - (c - 1.0));
-    CGFloat f2 = 1.0 / (2.0 + (c + 1.0));
+    CGFloat f1 = 2.0 / (3.0 - c);
+    CGFloat f2 = 2.0 / (3.0 + c);
     CGFloat tn = 1.0;
     CGFloat cotn = 1.0;
-    
+    NSRect extent1 = NSRectFromCGRect([inputImage extent]);
+    NSRect extent2 = NSRectFromCGRect([inputTargetImage extent]);
+
     if (direction % 2 == 0) {
         tn = tan(angle);
-        r = width;
+        r = 0.5 * width;
     } else {
         cotn = 1.0 / tan(angle);
-        r = height;
+        r = 0.5 * height;
     }
     
-    CGFloat x1 = f1 * (- width - r * s * cotn);
-    CGFloat x2 = f1 * (width - r * s * cotn);
-    CGFloat x3 = f2 * (- width + r * s * cotn);
-    CGFloat x4 = f2 * (width + r * s * cotn);
-    CGFloat y1 = f1 * (height - r * s * tn);
-    CGFloat y2 = f1 * (- height - r * s * tn);
-    CGFloat y3 = f2 * (height + r * s * tn);
-    CGFloat y4 = f2 * (- height + r * s * tn);
+    CGFloat x1 = f1 * (NSMinX(extent1) - x - r * s * cotn);
+    CGFloat x2 = f1 * (NSMaxX(extent1) - x - r * s * cotn);
+    CGFloat x3 = f2 * (NSMinX(extent2) - x + r * s * cotn);
+    CGFloat x4 = f2 * (NSMaxX(extent2) - x + r * s * cotn);
+    CGFloat y1 = f1 * (NSMaxY(extent1) - y - r * s * tn);
+    CGFloat y2 = f1 * (NSMinY(extent1) - y - r * s * tn);
+    CGFloat y3 = f2 * (NSMaxY(extent2) - y + r * s * tn);
+    CGFloat y4 = f2 * (NSMinY(extent2) - y + r * s * tn);
     
-    CIFilter *perspectiveFilter1 = [CIFilter filterWithName:@"CIPerspectiveTransformWithExtent"];
+    CIFilter *perspectiveFilter1 = [CIFilter filterWithName:@"CIPerspectiveTransform"];
     [perspectiveFilter1 setValue:inputImage forKey:kCIInputImageKey];
-    [perspectiveFilter1 setValue:inputExtent forKey:kCIInputExtentKey];
     [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x1 Y:y + y1] forKey:kCIInputTopLeftKey];
     [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x2 Y:y + y1] forKey:kCIInputTopRightKey];
     [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x1 Y:y + y2] forKey:kCIInputBottomLeftKey];
     [perspectiveFilter1 setValue:[CIVector vectorWithX:x + x2 Y:y + y2] forKey:kCIInputBottomRightKey];
     
-    CIFilter *perspectiveFilter2 = [CIFilter filterWithName:@"CIPerspectiveTransformWithExtent"];
+    CIFilter *perspectiveFilter2 = [CIFilter filterWithName:@"CIPerspectiveTransform"];
     [perspectiveFilter2 setValue:inputTargetImage forKey:kCIInputImageKey];
-    [perspectiveFilter2 setValue:inputExtent forKey:kCIInputExtentKey];
     [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x3 Y:y + y3] forKey:kCIInputTopLeftKey];
     [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x4 Y:y + y3] forKey:kCIInputTopRightKey];
     [perspectiveFilter2 setValue:[CIVector vectorWithX:x + x3 Y:y + y4] forKey:kCIInputBottomLeftKey];
