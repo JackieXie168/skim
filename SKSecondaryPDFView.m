14b7489e657f97f53f938783f2300b15151003c3
hofman <hofman@gmail.com>
2020-06-15 14:09:47 +0000

    ignore setAutoScales when loading document in secondary pdfview

--- SKSecondaryPDFView.m
+++ SKSecondaryPDFView.m
@@ -151,7 +151,10 @@ static CGFloat SKDefaultScaleMenuFactors[] = {0.0, 0.0, 0.1, 0.2, 0.25, 0.35, 0.
 - (void)setDocument:(PDFDocument *)document {
     if ([self document])
         [[NSNotificationCenter defaultCenter] removeObserver:self name:PDFDocumentDidUnlockNotification object:[self document]];
+    BOOL savedSwitching = switching;
+    switching = YES;
     [super setDocument:document];
+    switching = savedSwitching;
     [self reloadPagePopUpButton];
     if (document && [document isLocked])
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleDocumentDidUnlockNotification:) 
