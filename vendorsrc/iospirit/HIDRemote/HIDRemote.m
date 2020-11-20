9e934c840f40e753ce303417edfc74f6601a32ec
hofman <hofman@gmail.com>
2020-11-20 15:53:30 +0000

    Just comment out use of Gestalt in HIDRemote, we know we are on systems later than 10.6

--- vendorsrc/iospirit/HIDRemote/HIDRemote.m
+++ vendorsrc/iospirit/HIDRemote/HIDRemote.m
@@ -187,16 +187,14 @@ static HIDRemote *sHIDRemote = nil;
 
 + (BOOL)isCandelairInstallationRequiredForRemoteMode:(HIDRemoteMode)remoteMode
 {
-	SInt32 systemVersionMajor = 0, systemVersionMinor = 0, systemVersionBugFix;
+	/*
+    SInt32 systemVersionMajor = 0, systemVersionMinor = 0, systemVersionBugFix;
 	
 	// Determine OS version, use components as gestaltSystemVersion breaks on 10.10
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 	if (Gestalt(gestaltSystemVersionMajor, &systemVersionMajor) == noErr &&
 		Gestalt(gestaltSystemVersionMinor, &systemVersionMinor) == noErr &&
 		Gestalt(gestaltSystemVersionBugFix, &systemVersionBugFix) == noErr)
 	{
-#pragma clang diagnostic pop
         if (systemVersionMajor == 10 && systemVersionMinor == 6 && systemVersionBugFix <= 1)
 		{
 			// OS X 10.6(.0) and OS X 10.6.1 require the Candelair driver for to be installed,
@@ -217,7 +215,8 @@ static HIDRemote *sHIDRemote = nil;
 			}
 		}
 	}
-	
+	*/
+    
 	return (NO);
 }
 
@@ -1409,14 +1408,11 @@ static HIDRemote *sHIDRemote = nil;
 					{
 						if ([(NSString *)ioKitClassName isEqual:@"AppleIRController"])
 						{
-							SInt32 systemVersion;
+							//SInt32 systemVersion;
 							
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-							if (Gestalt(gestaltSystemVersion, &systemVersion) == noErr)
+							//if (Gestalt(gestaltSystemVersion, &systemVersion) == noErr)
 							{
-#pragma clang diagnostic pop
-                                if (systemVersion >= 0x1062)
+                                //if (systemVersion >= 0x1062)
 								{
 									// Support for the Aluminum Remote was added only with OS 10.6.2. Previous versions can not distinguish
 									// between the Center and the new, seperate Play/Pause button. They'll recognize both as presses of the
